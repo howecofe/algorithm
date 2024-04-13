7e44f517956aa6758c56a841c6813cc1f90db7d5
Greta Jiyun Lee <51826219+howecofe@users.noreply.github.com>
2024-04-13 17:02:23 +0900

    [level 0] Title: 두 수의 차, Time: 0.02 ms, Memory: 65.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120803. 두 수의 차/두 수의 차.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int num1, int num2) {
+        int answer = 0;
+        int max = 50000;
+        int min = -50000;
+        
+        if (min <= num1 && num1 <= max && min <= num2 && num2 <= max) {
+            return num1 - num2;
+        }
+        
+        return answer;
+    }
+}
