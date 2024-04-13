5510d9659f2d556740d6c32d7e9f65f4c1eef15b
Greta Jiyun Lee <51826219+howecofe@users.noreply.github.com>
2024-04-13 17:05:19 +0900

    [level 0] Title: 중앙값 구하기, Time: 0.02 ms, Memory: 73.7 MB -BaekjoonHub

--- 프로그래머스/0/120811. 중앙값 구하기/중앙값 구하기.java
+++ 프로그래머스/0/120811. 중앙값 구하기/중앙값 구하기.java
@@ -1,6 +1,10 @@
+import java.util.*;
+
 class Solution {
     public int solution(int[] array) {
         int answer = 0;
+        
+        // Arrays.sort(array);
         
         for (int i = 0; i < array.length; i++) {
             for (int j = i; j < array.length; j++) {
