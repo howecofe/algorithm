bf57a1c59e32f19c90cbad30732dec6fc7eca6bf
Greta Jiyun Lee <51826219+howecofe@users.noreply.github.com>
2024-04-13 12:49:23 +0900

    [level 0] Title: 두 수의 합, Time: 0.01 ms, Memory: 70.4 MB -BaekjoonHub

--- 프로그래머스/0/120802. 두 수의 합/두 수의 합.java
+++ 프로그래머스/0/120802. 두 수의 합/두 수의 합.java
@@ -1,5 +1,12 @@
 class Solution {
     public int solution(int num1, int num2) {
-        return num1 + num2;
+        int answer = -1;
+        int max = 50000;
+        int min = -50000;
+        
+        if (min <= num1 && num1 <= max && min <= num2 && num2 <= max) {
+            return num1 + num2;
+        }
+        return answer;
     }
 }
