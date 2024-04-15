781a68d22838d7ab601f62dcb708fd67fd19ff74
Greta Jiyun Lee <51826219+howecofe@users.noreply.github.com>
2024-04-16 03:14:59 +0900

    [level 0] Title: 모스부호 (1), Time: 9.30 ms, Memory: 77 MB -BaekjoonHub

--- 프로그래머스/0/120838. 모스부호 （1）/README.md
+++ 프로그래머스/0/120838. 모스부호 （1）/README.md
@@ -1,10 +1,10 @@
 # [level 0] 모스부호 (1) - 120838 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120838) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120838?language=java) 
 
 ### 성능 요약
 
-메모리: 86.5 MB, 시간: 12.71 ms
+메모리: 77 MB, 시간: 9.30 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 16일 03:11:23
+2024년 04월 16일 03:14:58
 
 ### 문제 설명
 
--- 프로그래머스/0/120838. 모스부호 （1）/모스부호 （1）.java
+++ 프로그래머스/0/120838. 모스부호 （1）/모스부호 （1）.java
@@ -1,14 +1,12 @@
-import java.util.*;
-
 class Solution {
     public String solution(String letter) {
         String answer = "";
         
-        List<String> morse = new ArrayList<>(Arrays.asList(".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."));
+        String[] morse = {".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
         
         for (String s : letter.split(" ")) {
-            for (int i = 0; i < morse.size(); i++) {
-                if (s.equals(morse.get(i))) {
+            for (int i = 0; i < morse.length; i++) {
+                if (s.equals(morse[i])) {
                     answer += (char)(i + (int)'a');
                 }                
             }
