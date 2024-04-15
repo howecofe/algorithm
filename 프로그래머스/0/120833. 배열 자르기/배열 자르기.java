54e9e5e90871e8c56b2d4c0a2569c04fa054c0cf
Greta Jiyun Lee <51826219+howecofe@users.noreply.github.com>
2024-04-15 23:45:08 +0900

    [level 0] Title: 배열 자르기, Time: 0.02 ms, Memory: 77.4 MB -BaekjoonHub

--- 프로그래머스/0/120833. 배열 자르기/배열 자르기.java
+++ 프로그래머스/0/120833. 배열 자르기/배열 자르기.java
@@ -1,19 +1,10 @@
-import java.util.*;
-
 class Solution {
     public int[] solution(int[] numbers, int num1, int num2) {
+        int[] answer = new int[num2 - num1 + 1];
         
-        List<Integer> list = new ArrayList<Integer>();
         for (int i = num1; i <= num2; i++) {
-            list.add(numbers[i]);
+            answer[i - num1] = numbers[i];
         }
-        
-        // List to int array
-        int[] answer = new int[list.size()];
-        for (int i = 0; i < list.size(); i++) {
-            answer[i] = list.get(i);
-        }
-        // answer = list.stream().mapToInt(Integer::intValue).toArray();
         
         return answer;
     }
