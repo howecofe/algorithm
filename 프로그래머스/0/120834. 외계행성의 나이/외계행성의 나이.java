cf6f3e72595f558cfd0e95975a90bae374cba70d
Greta Jiyun Lee <51826219+howecofe@users.noreply.github.com>
2024-04-16 00:39:31 +0900

    [level 0] Title: 외계행성의 나이, Time: 0.06 ms, Memory: 73.3 MB -BaekjoonHub

--- 프로그래머스/0/120834. 외계행성의 나이/외계행성의 나이.java
+++ 프로그래머스/0/120834. 외계행성의 나이/외계행성의 나이.java
@@ -1,27 +1,13 @@
-import java.util.*;
-
 class Solution {
     public String solution(int age) {
-        String answer = "";
+ 
+        StringBuilder sb = new StringBuilder();
         
-        Map<String, String> ageAlpha = new HashMap();
-        ageAlpha.put("0", "a");
-        ageAlpha.put("1", "b");
-        ageAlpha.put("2", "c");
-        ageAlpha.put("3", "d");
-        ageAlpha.put("4", "e");
-        ageAlpha.put("5", "f");
-        ageAlpha.put("6", "g");
-        ageAlpha.put("7", "h");
-        ageAlpha.put("8", "i");
-        ageAlpha.put("9", "j");
-        
-        String[] str = Integer.toString(age).split("");
-        
-        for (String s : str) {
-            answer += ageAlpha.get(s);
+        while (age > 0) { // 제한사항: age는 자연수이다.
+            sb.insert(0, (char) (age % 10 + (int)'a'));
+            age /= 10;
         }
         
-        return answer;
+        return sb.toString();
     }
 }
