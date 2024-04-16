a3260c42383a59f34d439e85e1578b68265e9d11
Greta Jiyun Lee <51826219+howecofe@users.noreply.github.com>
2024-04-16 23:19:53 +0900

    [level 0] Title: 암호 해독, Time: 2.23 ms, Memory: 78.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120892. 암호 해독/암호 해독.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String solution(String cipher, int code) {
+        String answer = "";
+        
+        String[] words = cipher.split("");
+        for (int i = 1; i <= words.length; i++) {
+            if (i % code == 0) answer += words[i - 1];
+        }
+        
+        return answer;
+    }
+}
