cca0dc07e0eba5220739262110621cf8af6f8c5e
Greta Jiyun Lee <51826219+howecofe@users.noreply.github.com>
2024-04-16 12:40:50 +0900

    [level 0] Title: 최댓값 만들기 (1), Time: 0.35 ms, Memory: 71 MB -BaekjoonHub

--- 프로그래머스/0/120847. 최댓값 만들기 （1）/README.md
+++ 프로그래머스/0/120847. 최댓값 만들기 （1）/README.md
@@ -1,10 +1,10 @@
 # [level 0] 최댓값 만들기 (1) - 120847 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120847) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120847?language=java) 
 
 ### 성능 요약
 
-메모리: 79.1 MB, 시간: 0.01 ms
+메모리: 71 MB, 시간: 0.35 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 16일 12:39:16
+2024년 04월 16일 12:40:48
 
 ### 문제 설명
 
--- 프로그래머스/0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.java
+++ 프로그래머스/0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.java
@@ -1,12 +1,16 @@
+import java.util.*;
+
 class Solution {
     public int solution(int[] numbers) {
-        int max = 0;
-        for (int i = 0; i < numbers.length; i++) {
-            for (int j = i + 1; j < numbers.length; j++) {
-                if (max < numbers[i] * numbers[j]) max = numbers[i] * numbers[j];
-            }
-        }
+        // int max = 0;
+        // for (int i = 0; i < numbers.length; i++) {
+        //     for (int j = i + 1; j < numbers.length; j++) {
+        //         if (max < numbers[i] * numbers[j]) max = numbers[i] * numbers[j];
+        //     }
+        // }
+        // return max;
         
-        return max;
+        Arrays.sort(numbers);
+        return numbers[numbers.length - 1] * numbers[numbers.length - 2];
     }
 }
