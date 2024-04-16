34da3b318e2f19be003fb1fddc6e565fe7783bd5
Greta Jiyun Lee <51826219+howecofe@users.noreply.github.com>
2024-04-16 23:23:07 +0900

    [level 0] Title: 암호 해독, Time: 1.40 ms, Memory: 73.2 MB -BaekjoonHub

--- 프로그래머스/0/120892. 암호 해독/README.md
+++ 프로그래머스/0/120892. 암호 해독/README.md
@@ -1,10 +1,10 @@
 # [level 0] 암호 해독 - 120892 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120892) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120892?language=java) 
 
 ### 성능 요약
 
-메모리: 78.9 MB, 시간: 2.23 ms
+메모리: 73.2 MB, 시간: 1.40 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 16일 23:19:52
+2024년 04월 16일 23:23:06
 
 ### 문제 설명
 
--- 프로그래머스/0/120892. 암호 해독/암호 해독.java
+++ 프로그래머스/0/120892. 암호 해독/암호 해독.java
@@ -2,9 +2,13 @@ class Solution {
     public String solution(String cipher, int code) {
         String answer = "";
         
-        String[] words = cipher.split("");
-        for (int i = 1; i <= words.length; i++) {
-            if (i % code == 0) answer += words[i - 1];
+        // String[] words = cipher.split("");
+        // for (int i = 1; i <= words.length; i++) {
+        //     if (i % code == 0) answer += words[i - 1];
+        // }
+
+        for (int i = code; i <= cipher.length(); i = i + code) {
+            answer += cipher.substring(i - 1, i);
         }
         
         return answer;
