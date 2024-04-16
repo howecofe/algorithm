c94a5d9e50fed847c3cd895ff5060290b95ad5d4
Greta Jiyun Lee <51826219+howecofe@users.noreply.github.com>
2024-04-17 04:19:46 +0900

    [level 0] Title: 공 던지기, Time: 0.03 ms, Memory: 82.8 MB -BaekjoonHub

--- 프로그래머스/0/120843. 공 던지기/README.md
+++ 프로그래머스/0/120843. 공 던지기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 공 던지기 - 120843 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120843) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120843?language=java) 
 
 ### 성능 요약
 
-메모리: 72.9 MB, 시간: 0.05 ms
+메모리: 82.8 MB, 시간: 0.03 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 17일 04:01:08
+2024년 04월 17일 04:19:44
 
 ### 문제 설명
 
--- 프로그래머스/0/120843. 공 던지기/공 던지기.java
+++ 프로그래머스/0/120843. 공 던지기/공 던지기.java
@@ -2,18 +2,19 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] numbers, int k) {
-        List<Integer> order = new ArrayList<>();
+//         List<Integer> order = new ArrayList<>();
         
-        for (int i = 0; i < numbers.length; i += 2) {
-                order.add(numbers[i]);
-        }
+//         for (int i = 0; i < numbers.length; i += 2) {
+//                 order.add(numbers[i]);
+//         }
         
-        if (numbers.length % 2 == 1) {
-            for (int i = 1; i < numbers.length; i += 2) {
-                order.add(numbers[i]);
-            }
-        }
+//         if (numbers.length % 2 == 1) {
+//             for (int i = 1; i < numbers.length; i += 2) {
+//                 order.add(numbers[i]);
+//             }
+//         }
         
-        return order.get((k - 1) % order.size());
+//         return order.get((k - 1) % order.size());
+        return numbers[(k - 1) * 2 % numbers.length];
     }
 }
