76a59b6eb5048732381eb4ed9b5fa38e2b241c3d
Greta Jiyun Lee <51826219+howecofe@users.noreply.github.com>
2024-04-17 04:01:09 +0900

    [level 0] Title: 공 던지기, Time: 0.05 ms, Memory: 72.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120843. 공 던지기/공 던지기.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] numbers, int k) {
+        List<Integer> order = new ArrayList<>();
+        
+        for (int i = 0; i < numbers.length; i += 2) {
+                order.add(numbers[i]);
+        }
+        
+        if (numbers.length % 2 == 1) {
+            for (int i = 1; i < numbers.length; i += 2) {
+                order.add(numbers[i]);
+            }
+        }
+        
+        return order.get((k - 1) % order.size());
+    }
+}
