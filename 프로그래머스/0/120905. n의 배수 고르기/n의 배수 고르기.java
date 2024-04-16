22a9d0f097b602c7072b71974b703bde0d15c486
Greta Jiyun Lee <51826219+howecofe@users.noreply.github.com>
2024-04-17 00:29:25 +0900

    [level 0] Title: n의 배수 고르기, Time: 0.03 ms, Memory: 78 MB -BaekjoonHub

--- 프로그래머스/0/120905. n의 배수 고르기/n의 배수 고르기.java
+++ 프로그래머스/0/120905. n의 배수 고르기/n의 배수 고르기.java
@@ -1,23 +1,20 @@
 class Solution {
     public int[] solution(int n, int[] numlist) {
-        int size = numlist.length;
-        
-        // n의 배수가 아닌 값을 0으로 만들기
-        for (int i = 0; i < numlist.length; i ++) {
-            if (numlist[i] % n != 0) {
-                numlist[i] = 0;
-                size--;
+        int cnt = 0;
+
+        for (int num : numlist) {
+            if (num % n == 0) {
+                cnt++;
             }
         }
-        
-        // 0이 아닌 값(n의 배수)을 새로운 배열에 저장하기
-        int[] answer = new int[size];
+
+        int[] answer = new int[cnt];
         int idx = 0;
-        for (int j = 0; j < numlist.length; j++) {
-            if (idx == size) break;
+        for (int num : numlist) {
+            if (idx == cnt) break;
             
-            if (numlist[j] != 0) {
-                answer[idx] = numlist[j];
+            if (num % n == 0) {
+                answer[idx] = num;
                 idx++;
             }      
         }
