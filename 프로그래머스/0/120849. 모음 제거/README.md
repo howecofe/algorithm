45ae110ce513e3b085d283002c517cbcddf44bd6
Greta Jiyun Lee <51826219+howecofe@users.noreply.github.com>
2024-04-16 13:38:25 +0900

    [level 0] Title: 모음 제거, Time: 0.14 ms, Memory: 76.7 MB -BaekjoonHub

--- 프로그래머스/0/120849. 모음 제거/README.md
+++ 프로그래머스/0/120849. 모음 제거/README.md
@@ -1,10 +1,10 @@
 # [level 0] 모음 제거 - 120849 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120849) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120849?language=java) 
 
 ### 성능 요약
 
-메모리: 70.4 MB, 시간: 1.35 ms
+메모리: 76.7 MB, 시간: 0.14 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 16일 13:36:10
+2024년 04월 16일 13:38:23
 
 ### 문제 설명
 
--- 프로그래머스/0/120849. 모음 제거/모음 제거.java
+++ 프로그래머스/0/120849. 모음 제거/모음 제거.java
@@ -1,19 +1,20 @@
 class Solution {
     public String solution(String my_string) {
-        String answer = "";
-        String[] vowl = {"a", "e", "i", "o", "u"};
+//         String answer = "";
+//         String[] vowel = {"a", "e", "i", "o", "u"};
         
-        for (String str : my_string.split("")) {
-            boolean isVowl = false;
-            for (String v : vowl) {
-                if (str.equals(v)) {
-                    isVowl = !isVowl;
-                    break;
-                }
-            }
-            if (!isVowl) answer += str;
-        }
+//         for (String str : my_string.split("")) {
+//             boolean isVowel = false;
+//             for (String v : vowel) {
+//                 if (str.equals(v)) {
+//                     isVowel = !isVowel;
+//                     break;
+//                 }
+//             }
+//             if (!isVowel) answer += str;
+//         }
         
-        return answer;
+//         return answer;
+        return my_string.replaceAll("[aeiou]", "");
     }
 }
