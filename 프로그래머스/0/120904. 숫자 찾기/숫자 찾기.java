72430993edd89ea25dc6bf92d9544cfa33b021a4
Greta Jiyun Lee <51826219+howecofe@users.noreply.github.com>
2024-04-17 21:24:00 +0900

    [level 0] Title: 숫자 찾기, Time: 0.02 ms, Memory: 80.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int num, int k) {
+        int answer = -1;
+        List<Integer> list = new ArrayList<>();
+        
+        while (num != 0) {
+            list.add(num % 10);
+            num /= 10;
+        }
+        
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i) == k) {
+                answer = list.size() - i;
+            }
+        }
+        
+        return answer;
+    }
+}
