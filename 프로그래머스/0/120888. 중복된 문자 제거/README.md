2705d2cb791bfcb9b917ba0cae8009f58f11d6dd
Greta Jiyun Lee <51826219+howecofe@users.noreply.github.com>
2024-04-18 01:36:25 +0900

    [level 0] Title: 중복된 문자 제거, Time: 11.80 ms, Memory: 76.7 MB -BaekjoonHub

--- 프로그래머스/0/120888. 중복된 문자 제거/README.md
+++ 프로그래머스/0/120888. 중복된 문자 제거/README.md
@@ -1,10 +1,10 @@
 # [level 0] 중복된 문자 제거 - 120888 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120888) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120888?language=java) 
 
 ### 성능 요약
 
-메모리: 73.1 MB, 시간: 1.68 ms
+메모리: 76.7 MB, 시간: 11.80 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 18일 01:25:30
+2024년 04월 18일 01:36:24
 
 ### 문제 설명
 
--- 프로그래머스/0/120888. 중복된 문자 제거/중복된 문자 제거.java
+++ 프로그래머스/0/120888. 중복된 문자 제거/중복된 문자 제거.java
@@ -4,13 +4,27 @@ class Solution {
     public String solution(String my_string) {
         String answer = "";
         
-        LinkedHashSet<String> set = new LinkedHashSet<>();
-        for (String str : my_string.split("")) {
-            set.add(str);
-        }
+        // 방법 1
+//         LinkedHashSet<String> set = new LinkedHashSet<>();
+//         for (String str : my_string.split("")) {
+//             set.add(str);
+//         }
         
-        for (String data : set) {
-            answer += data;
+//         for (String data : set) {
+//             answer += data;
+//         }
+        
+        
+        for (int i = 0; i < my_string.length(); i++) {
+            // 방법 2
+            if (i == my_string.indexOf(my_string.charAt(i))) {
+                answer += my_string.charAt(i);
+            }
+            
+            // 방법 3
+            // if (!answer.contains(String.valueOf(my_string.charAt(i)))) {
+            //     answer += my_string.charAt(i);
+            // }
         }
         
         return answer;
