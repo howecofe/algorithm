9b6e883d4e5e31baece538ef01312236d7de2c90
Greta Jiyun Lee <51826219+howecofe@users.noreply.github.com>
2024-04-17 21:26:08 +0900

    [level 0] Title: 숫자 찾기, Time: 5.47 ms, Memory: 76.1 MB -BaekjoonHub

--- 프로그래머스/0/120904. 숫자 찾기/README.md
+++ 프로그래머스/0/120904. 숫자 찾기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 숫자 찾기 - 120904 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120904) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120904?language=java) 
 
 ### 성능 요약
 
-메모리: 80.9 MB, 시간: 0.02 ms
+메모리: 76.1 MB, 시간: 5.47 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 17일 21:23:58
+2024년 04월 17일 21:26:06
 
 ### 문제 설명
 
--- 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.java
+++ 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.java
@@ -1,21 +1,22 @@
-import java.util.*;
+// import java.util.*;
 
 class Solution {
     public int solution(int num, int k) {
-        int answer = -1;
-        List<Integer> list = new ArrayList<>();
+//         int answer = -1;
+//         List<Integer> list = new ArrayList<>();
         
-        while (num != 0) {
-            list.add(num % 10);
-            num /= 10;
-        }
+//         while (num != 0) {
+//             list.add(num % 10);
+//             num /= 10;
+//         }
         
-        for (int i = 0; i < list.size(); i++) {
-            if (list.get(i) == k) {
-                answer = list.size() - i;
-            }
-        }
+//         for (int i = 0; i < list.size(); i++) {
+//             if (list.get(i) == k) {
+//                 answer = list.size() - i;
+//             }
+//         }
         
-        return answer;
+//         return answer;
+        return ("-" + num).indexOf(String.valueOf(k));
     }
 }
