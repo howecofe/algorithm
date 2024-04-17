cbfc464d2989924ad7e2dc01926a5fc608c934bb
Greta Jiyun Lee <51826219+howecofe@users.noreply.github.com>
2024-04-18 02:53:28 +0900

    [level 0] Title: 가까운 수, Time: 0.44 ms, Memory: 77.5 MB -BaekjoonHub

--- 프로그래머스/0/120890. 가까운 수/README.md
+++ 프로그래머스/0/120890. 가까운 수/README.md
@@ -1,10 +1,10 @@
 # [level 0] 가까운 수 - 120890 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120890) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120890?language=java) 
 
 ### 성능 요약
 
-메모리: 68.3 MB, 시간: 0.35 ms
+메모리: 77.5 MB, 시간: 0.44 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 18일 02:49:42
+2024년 04월 18일 02:53:27
 
 ### 문제 설명
 
--- 프로그래머스/0/120890. 가까운 수/가까운 수.java
+++ 프로그래머스/0/120890. 가까운 수/가까운 수.java
@@ -4,22 +4,12 @@ class Solution {
     public int solution(int[] array, int n) {
         Arrays.sort(array);
         
-        // array 요소와 n의 차이를 저장한다.
-        int[] diff = new int[array.length];
         for (int i = 0; i < array.length; i++) {
-            diff[i] = Math.abs(n - array[i]);
-        }
-        
-        // 차이가 최소인 값의 인덱스를 찾는다.
-        int min = diff[0];
-        int idx = 0;
-        for (int i = 0; i < diff.length; i++) {
-            if (min > diff[i]) { 
-                min = diff[i];
-                idx = i;
+            if (Math.abs(n - array[0]) > Math.abs(n - array[i])) {
+                array[0] = array[i];
             }
         }
         
-        return array[idx];
+        return array[0];
     }
 }
