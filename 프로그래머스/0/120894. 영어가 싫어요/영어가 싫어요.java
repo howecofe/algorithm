798ecc82736417b75f34cb415a7060014bac8210
Greta Jiyun Lee <51826219+howecofe@users.noreply.github.com>
2024-04-20 22:29:33 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.16 ms, Memory: 71.8 MB -BaekjoonHub

--- 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.java
+++ 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.java
@@ -1,14 +1,11 @@
-import java.util.*;
 class Solution {
     public long solution(String numbers) {
-        long answer = 0;
         String[] alpha = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
-        
-        String str = numbers;
+    
         for (int i = 0 ; i < alpha.length; i++) {
-            str = str.replace(alpha[i], String.valueOf(i));
+            numbers = numbers.replace(alpha[i], String.valueOf(i));
         }
         
-        return Long.parseLong(str);
+        return Long.parseLong(numbers);
     }
 }
