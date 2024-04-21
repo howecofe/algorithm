f15d9ce7091aaf1d28b6a281882246b37380b208
Greta Jiyun Lee <51826219+howecofe@users.noreply.github.com>
2024-04-21 17:30:01 +0900

    [level 0] Title: 문자열 밀기, Time: 0.03 ms, Memory: 78.2 MB -BaekjoonHub

--- 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.java
+++ 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.java
@@ -1,27 +1,28 @@
 class Solution {
     public int solution(String A, String B) {
-        if (A.equals(B)) { return 0; }
+//         if (A.equals(B)) { return 0; }
         
-        for (int cnt = 1; cnt < A.length(); cnt++) {
-            String[] before = A.split("");
-            String[] after = new String[A.length()];
-            String lastStr = before[before.length - 1];
-            for (int i = 0; i < before.length; i++) {
-                if (i != before.length - 1) {
-                    after[i + 1] = before[i];
-                } else {
-                    after[0] = lastStr;
-                }
-            }
+//         for (int cnt = 1; cnt < A.length(); cnt++) {
+//             String[] before = A.split("");
+//             String[] after = new String[A.length()];
+//             String lastStr = before[before.length - 1];
+//             for (int i = 0; i < before.length; i++) {
+//                 if (i != before.length - 1) {
+//                     after[i + 1] = before[i];
+//                 } else {
+//                     after[0] = lastStr;
+//                 }
+//             }
             
-            A = "";
-            for (int i = 0; i < after.length; i++) {
-                A += after[i];
-            }
+//             A = "";
+//             for (int i = 0; i < after.length; i++) {
+//                 A += after[i];
+//             }
             
-            if (A.equals(B)) { return cnt; }
-        }
+//             if (A.equals(B)) { return cnt; }
+//         }
         
-        return -1;
+//         return -1;
+        return B.repeat(2).indexOf(A);
     }
 }
