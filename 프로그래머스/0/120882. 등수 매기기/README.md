b13cc45906287f3f697ab6fe7c4d37f6aaf7ec09
Greta Jiyun Lee <51826219+howecofe@users.noreply.github.com>
2024-04-21 13:42:58 +0900

    [level 0] Title: 등수 매기기, Time: 0.37 ms, Memory: 74.1 MB -BaekjoonHub

--- 프로그래머스/0/120882. 등수 매기기/README.md
+++ 프로그래머스/0/120882. 등수 매기기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 등수 매기기 - 120882 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120882) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120882?language=java) 
 
 ### 성능 요약
 
-메모리: 72.5 MB, 시간: 1.17 ms
+메모리: 74.1 MB, 시간: 0.37 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 21일 13:28:14
+2024년 04월 21일 13:42:56
 
 ### 문제 설명
 
--- 프로그래머스/0/120882. 등수 매기기/등수 매기기.java
+++ 프로그래머스/0/120882. 등수 매기기/등수 매기기.java
@@ -3,30 +3,16 @@ import java.util.*;
 class Solution {
     public int[] solution(int[][] score) {
         int[] answer = new int[score.length];
-        int[] sum = new int[score.length];
- 
-        String usedIdx = "";
+        List<Integer> scoreList = new ArrayList<>();
+        
         for (int i = 0; i < score.length; i++) {
-            int max = 0;
-            int maxIdx = 0;
-            for (int j = 0; j < score.length; j++) {
-                if (!usedIdx.contains(String.valueOf(j))) {
-                    if (max < score[j][0] + score[j][1]) {
-                        max = score[j][0] + score[j][1];
-                    }
-                }
-            }
-            
-            int cnt = 0;
-            for (int j = 0; j < score.length; j++) {
-                if (max == score[j][0] + score[j][1]) {
-                    maxIdx = j;
-                    answer[maxIdx] = i + 1;
-                    usedIdx += String.valueOf(maxIdx);
-                    cnt++;
-                }
-            }
-            i += cnt - 1;
+            scoreList.add(score[i][0] + score[i][1]);
+        }
+        
+        scoreList.sort(Comparator.reverseOrder());
+        
+        for (int i = 0; i < score.length; i++) {
+            answer[i] = scoreList.indexOf(score[i][0] + score[i][1]) + 1;
         }
             
         return answer;
