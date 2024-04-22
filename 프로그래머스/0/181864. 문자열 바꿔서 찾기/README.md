d1700d92f2757a1eff46681bac43fbe7cea7bd7e
Greta Jiyun Lee <51826219+howecofe@users.noreply.github.com>
2024-04-22 22:10:40 +0900

    [level 0] Title: 문자열 바꿔서 찾기, Time: 0.06 ms, Memory: 70.8 MB -BaekjoonHub

--- 프로그래머스/0/181864. 문자열 바꿔서 찾기/README.md
+++ 프로그래머스/0/181864. 문자열 바꿔서 찾기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 문자열 바꿔서 찾기 - 181864 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181864) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181864?language=java) 
 
 ### 성능 요약
 
-메모리: 72.3 MB, 시간: 2.85 ms
+메모리: 70.8 MB, 시간: 0.06 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 22일 22:08:37
+2024년 04월 22일 22:10:39
 
 ### 문제 설명
 
--- 프로그래머스/0/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.java
+++ 프로그래머스/0/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.java
@@ -1,13 +1,16 @@
 class Solution {
     public int solution(String myString, String pat) {
-        String str = "";
-        for (String s : myString.split("")) {
-            if (s.equals("A")) { str += "B"; }
-            else if (s.equals("B")) { str += "A"; }
-            else { str += s; }
-        }
+//         String str = "";
+//         for (String s : myString.split("")) {
+//             if (s.equals("A")) { str += "B"; }
+//             else if (s.equals("B")) { str += "A"; }
+//             else { str += s; }
+//         }
         
-        if (str.contains(pat)) { return 1; }
-        else { return 0;}
+//         if (str.contains(pat)) { return 1; }
+//         else { return 0;}
+        
+        myString = myString.replace("A", "a").replace("B", "A").replace("a", "B");
+        return myString.contains(pat) ? 1 : 0;
     }
 }
