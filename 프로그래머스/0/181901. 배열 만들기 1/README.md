8b4c379d48d2424f78889c0a2a63c5fc8fb501b0
Greta Jiyun Lee <51826219+howecofe@users.noreply.github.com>
2024-04-23 19:36:42 +0900

    [level 0] Title: 배열 만들기 1, Time: 0.02 ms, Memory: 77.5 MB -BaekjoonHub

--- 프로그래머스/0/181901. 배열 만들기 1/README.md
+++ 프로그래머스/0/181901. 배열 만들기 1/README.md
@@ -1,10 +1,10 @@
 # [level 0] 배열 만들기 1 - 181901 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181901) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181901?language=java) 
 
 ### 성능 요약
 
-메모리: 84.2 MB, 시간: 2.60 ms
+메모리: 77.5 MB, 시간: 0.02 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 23일 19:34:14
+2024년 04월 23일 19:36:40
 
 ### 문제 설명
 
--- 프로그래머스/0/181901. 배열 만들기 1/배열 만들기 1.java
+++ 프로그래머스/0/181901. 배열 만들기 1/배열 만들기 1.java
@@ -2,17 +2,23 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int n, int k) {
-        List<Integer> list = new ArrayList<>();
+//         List<Integer> list = new ArrayList<>();
         
-        for (int i = 1; i <= n; i++) {
-            if (i % k == 0) { list.add(i); }
+//         for (int i = 1; i <= n; i++) {
+//             if (i % k == 0) { list.add(i); }
+//         }
+//         list.sort(Comparator.naturalOrder());
+//         Integer[] integerArr = list.toArray(new Integer[0]);
+//         int[] arr = new int[integerArr.length];
+//         for (int i = 0; i < arr.length; i++) {
+//             arr[i] = integerArr[i];
+//         }
+//         return arr;
+        
+        int[] answer = new int[n / k];
+        for (int i = 0; i < answer.length; i++) {
+            answer[i] = (i + 1) * k;
         }
-        list.sort(Comparator.naturalOrder());
-        Integer[] integerArr = list.toArray(new Integer[0]);
-        int[] arr = new int[integerArr.length];
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = integerArr[i];
-        }
-        return arr;
+        return answer;
     }
 }
