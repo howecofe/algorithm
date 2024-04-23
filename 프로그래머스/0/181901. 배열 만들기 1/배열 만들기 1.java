ec9e7ccac216211a11c9ea0b32577f340b77ff38
Greta Jiyun Lee <51826219+howecofe@users.noreply.github.com>
2024-04-23 19:34:16 +0900

    [level 0] Title: 배열 만들기 1, Time: 2.60 ms, Memory: 84.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181901. 배열 만들기 1/배열 만들기 1.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int n, int k) {
+        List<Integer> list = new ArrayList<>();
+        
+        for (int i = 1; i <= n; i++) {
+            if (i % k == 0) { list.add(i); }
+        }
+        list.sort(Comparator.naturalOrder());
+        Integer[] integerArr = list.toArray(new Integer[0]);
+        int[] arr = new int[integerArr.length];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = integerArr[i];
+        }
+        return arr;
+    }
+}
