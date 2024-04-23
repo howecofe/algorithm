05cf0887e1b379905aee89998d24c6f1cf708c58
Greta Jiyun Lee <51826219+howecofe@users.noreply.github.com>
2024-04-23 10:43:23 +0900

    [level 0] Title: 5명씩, Time: 0.02 ms, Memory: 78.4 MB -BaekjoonHub

--- 프로그래머스/0/181886. 5명씩/5명씩.java
+++ 프로그래머스/0/181886. 5명씩/5명씩.java
@@ -1,11 +1,9 @@
-import java.util.*;
-
 class Solution {
     public String[] solution(String[] names) {
-        List<String> list = new ArrayList<>();
-        for (int i = 0; i < names.length; i += 5) {
-            list.add(names[i]);
+        String[] answer = new String[names.length % 5 == 0 ? names.length / 5 : names.length / 5 + 1];
+        for (int i = 0; i < answer.length; i++) {
+            answer[i] = names[i * 5];
         }
-        return list.toArray(new String[0]);
+        return answer;
     }
 }
