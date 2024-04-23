c2f6ca37a9219415e922b3ba32f6d71b76350bcd
Greta Jiyun Lee <51826219+howecofe@users.noreply.github.com>
2024-04-23 09:54:35 +0900

    [level 0] Title: A 강조하기, Time: 11.30 ms, Memory: 74.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181874. A 강조하기/A 강조하기.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String solution(String myString) {
+        String answer = "";
+    
+        myString = myString.replace("a", "A");
+        for (char c : myString.toCharArray()) {
+            if ('B' <= c && c <= 'Z') {
+                answer += Character.toLowerCase(c);
+            } else {
+                answer += c;
+            }
+        }
+        
+        return answer;
+    }
+}
