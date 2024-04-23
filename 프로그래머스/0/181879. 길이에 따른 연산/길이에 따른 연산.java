7a4ef931936c314d760045e2bec2407c9475b204
Greta Jiyun Lee <51826219+howecofe@users.noreply.github.com>
2024-04-23 10:06:46 +0900

    [level 0] Title: 길이에 따른 연산, Time: 0.02 ms, Memory: 76.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181879. 길이에 따른 연산/길이에 따른 연산.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int[] num_list) {
+        int sum = 0;
+        int mul = 1;
+        
+        for (int num : num_list) {
+            sum += num;
+            mul *= num;
+        }
+        
+        return num_list.length > 10 ? sum : mul;
+    }
+}
