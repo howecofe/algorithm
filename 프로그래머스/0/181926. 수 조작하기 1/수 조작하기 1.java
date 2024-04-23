937373ad7a862bf0bc22cb09a28e15ebed092bcf
Greta Jiyun Lee <51826219+howecofe@users.noreply.github.com>
2024-04-24 00:30:43 +0900

    [level 0] Title: 수 조작하기 1, Time: 12.99 ms, Memory: 81.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int n, String control) {
+        for (String s : control.split("")) {
+            if (s.equals("w")) { n++; }
+            else if (s.equals("s")) { n--; }
+            else if (s.equals("d")) { n += 10; }
+            else { n -= 10; }
+        }
+        return n;
+    }
+}
