6ebc7e757d31b0408065e131a447d41132aa8ecd
Greta Jiyun Lee <51826219+howecofe@users.noreply.github.com>
2024-04-23 09:50:04 +0900

    [level 0] Title: 특정한 문자를 대문자로 바꾸기, Time: 0.03 ms, Memory: 74.7 MB -BaekjoonHub

--- 프로그래머스/0/181873. 특정한 문자를 대문자로 바꾸기/README.md
+++ 프로그래머스/0/181873. 특정한 문자를 대문자로 바꾸기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 특정한 문자를 대문자로 바꾸기 - 181873 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181873) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181873?language=java) 
 
 ### 성능 요약
 
-메모리: 74.8 MB, 시간: 1.62 ms
+메모리: 74.7 MB, 시간: 0.03 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 23일 09:48:10
+2024년 04월 23일 09:50:02
 
 ### 문제 설명
 
--- 프로그래머스/0/181873. 특정한 문자를 대문자로 바꾸기/특정한 문자를 대문자로 바꾸기.java
+++ 프로그래머스/0/181873. 특정한 문자를 대문자로 바꾸기/특정한 문자를 대문자로 바꾸기.java
@@ -1,14 +1,16 @@
 class Solution {
     public String solution(String my_string, String alp) {
-        String answer = "";
+//         String answer = "";
         
-        for (String s : my_string.split("")) {
-            if (s.equals(alp)) {
-                answer += s.toUpperCase();
-            } else {
-                answer += s;
-            }
-        }
-        return answer;
+//         for (String s : my_string.split("")) {
+//             if (s.equals(alp)) {
+//                 answer += s.toUpperCase();
+//             } else {
+//                 answer += s;
+//             }
+//         }
+//         return answer;
+
+        return my_string.replace(alp, alp.toUpperCase());
     }
 }
