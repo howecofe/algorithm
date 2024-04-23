ef913809993f053c8e90bbf4ab7a3e7d32ee61b7
Greta Jiyun Lee <51826219+howecofe@users.noreply.github.com>
2024-04-23 23:36:09 +0900

    [level 0] Title: 0 떼기, Time: 7.58 ms, Memory: 75.4 MB -BaekjoonHub

--- 프로그래머스/0/181847. 0 떼기/0 떼기.java
+++ 프로그래머스/0/181847. 0 떼기/0 떼기.java
@@ -1,11 +1,13 @@
 class Solution {
     public String solution(String n_str) {
-        while (n_str.charAt(0) == '0') {
-            String answer = "";
-            for (int i = 1; i < n_str.length(); i++) { answer += n_str.charAt(i); }
-            n_str = answer;
-        }
+//         while (n_str.charAt(0) == '0') {
+//             String answer = "";
+//             for (int i = 1; i < n_str.length(); i++) { answer += n_str.charAt(i); }
+//             n_str = answer;
+//         }
         
-        return n_str;
+//         return n_str;
+        
+        return "" + Integer.parseInt(n_str);
     }   
 }
--- 프로그래머스/0/181847. 0 떼기/README.md
+++ 프로그래머스/0/181847. 0 떼기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 0 떼기 - 181847 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181847) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181847?language=java) 
 
 ### 성능 요약
 
-메모리: 79.6 MB, 시간: 0.02 ms
+메모리: 75.4 MB, 시간: 7.58 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 23일 23:35:18
+2024년 04월 23일 23:36:07
 
 ### 문제 설명
 
