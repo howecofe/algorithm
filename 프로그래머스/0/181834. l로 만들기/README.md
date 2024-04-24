21e826bdb838d9859ad29d743323945cfedb94ea
Greta Jiyun Lee <51826219+howecofe@users.noreply.github.com>
2024-04-24 12:57:35 +0900

    [level 0] Title: l로 만들기, Time: 20.65 ms, Memory: 86.8 MB -BaekjoonHub

--- 프로그래머스/0/181834. l로 만들기/README.md
+++ 프로그래머스/0/181834. l로 만들기/README.md
@@ -1,10 +1,10 @@
 # [level 0] l로 만들기 - 181834 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181834) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181834?language=java) 
 
 ### 성능 요약
 
-메모리: 384 MB, 시간: 697.00 ms
+메모리: 86.8 MB, 시간: 20.65 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 24일 12:54:08
+2024년 04월 24일 12:57:33
 
 ### 문제 설명
 
--- 프로그래머스/0/181834. l로 만들기/l로 만들기.java
+++ 프로그래머스/0/181834. l로 만들기/l로 만들기.java
@@ -1,10 +1,11 @@
 class Solution {
     public String solution(String myString) {
-        String answer = "";
-        for (char c : myString.toCharArray()) {
-            if ('a' <= c && c < 'l') { answer += 'l'; }
-            else { answer += c; }
-        }
-        return answer;
+        // String answer = "";
+        // for (char c : myString.toCharArray()) {
+        //     if ('a' <= c && c < 'l') { answer += 'l'; }
+        //     else { answer += c; }
+        // }
+        // return answer;
+        return myString.replaceAll("[^l-z]", "l");
     }
 }
