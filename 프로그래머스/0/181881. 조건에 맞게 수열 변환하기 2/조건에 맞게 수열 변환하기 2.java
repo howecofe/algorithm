20b8d0147d08749a27e73ecf9088fc7ba9aceceb
Greta Jiyun Lee <51826219+howecofe@users.noreply.github.com>
2024-04-24 22:01:43 +0900

    [level 0] Title: 조건에 맞게 수열 변환하기 2, Time: 0.03 ms, Memory: 77.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181881. 조건에 맞게 수열 변환하기 2/조건에 맞게 수열 변환하기 2.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] arr) {
+        int arr1[] = new int[arr.length];
+        int x = 0;
+        int[] next = {};
+        int[] prev = Arrays.copyOf(arr, arr.length);
+        
+        while (true) {
+            x++;
+            boolean sameArr = true;
+            
+            next = Arrays.copyOf(prev, prev.length);
+            for (int i = 0; i < next.length; i++) {
+                if (prev[i] >= 50 && prev[i] % 2 == 0) {
+                    next[i] = prev[i] / 2 <= 100 ? prev[i] / 2 : prev[i];
+                } else if (prev[i] < 50 && prev[i] % 2 == 1) {
+                    next[i] = prev[i] * 2 + 1 <= 100 ? prev[i] * 2 + 1 : prev[i];
+                }
+            }
+            
+            for (int i = 0; i < next.length; i++) {
+                if (next[i] != prev[i]) { sameArr = false; break; }
+            }
+            
+            if (sameArr) { break; }
+            prev = Arrays.copyOf(next, next.length);
+        }
+        
+        return x - 1;
+    }
+}
