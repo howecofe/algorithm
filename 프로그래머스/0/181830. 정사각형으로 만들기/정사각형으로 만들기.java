2a53b9f7f5c8b322df6e762d6838f70544b78866
Greta Jiyun Lee <51826219+howecofe@users.noreply.github.com>
2024-04-25 01:56:40 +0900

    [level 0] Title: 정사각형으로 만들기, Time: 0.27 ms, Memory: 81 MB -BaekjoonHub

--- 프로그래머스/0/181830. 정사각형으로 만들기/정사각형으로 만들기.java
+++ 프로그래머스/0/181830. 정사각형으로 만들기/정사각형으로 만들기.java
@@ -5,13 +5,21 @@ class Solution {
         int max = Math.max(row, col);
         int[][] answer = new int[max][max];
         
-        for (int i = 0; i < max; i++) {
-            for (int j = 0; j < max; j++) {
-                if (i < row && j < col) { answer[i][j] = arr[i][j]; }
-                else { answer[i][j] = 0; } // 현재 인덱스가 arr 배열의 크기를 벗어나면 0으로 채우기
+        // for (int i = 0; i < max; i++) {
+        //     for (int j = 0; j < max; j++) {
+        //         if (i < row && j < col) { answer[i][j] = arr[i][j]; }
+        //         else { answer[i][j] = 0; } // 현재 인덱스가 arr 배열의 크기를 벗어나면 0으로 채우기
+        //     }
+        // }
+        
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                answer[i][j] = arr[i][j];
             }
         }
 
         return answer;
     }
 }
+
+// [Note] 배열은 기본적으로 0으로 채워짐..
