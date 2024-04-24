862c747d4afae2ea7a472483d839e4e86d83f8b8
Greta Jiyun Lee <51826219+howecofe@users.noreply.github.com>
2024-04-24 13:23:19 +0900

    [level 0] Title: 문자열 뒤집기, Time: 11.53 ms, Memory: 69.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181905. 문자열 뒤집기/문자열 뒤집기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String solution(String my_string, int s, int e) {
+        StringBuilder sb = new StringBuilder();
+        String s1 = "";
+        String s2 = "";
+        for (int i = 0; i < my_string.length(); i++) {
+            if (i < s) { s1 += my_string.charAt(i); }
+            else if (s <= i && i <= e) { sb.insert(0, my_string.charAt(i)); }
+            else { s2 += my_string.charAt(i); }
+        }
+        return s1 + sb.toString() + s2;
+    }
+}
