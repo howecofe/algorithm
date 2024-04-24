8f9db80f7710c254d751b1f5a17515f2294b6fea
Greta Jiyun Lee <51826219+howecofe@users.noreply.github.com>
2024-04-24 20:30:38 +0900

    [level 0] Title: 리스트 자르기, Time: 0.02 ms, Memory: 76.3 MB -BaekjoonHub

--- 프로그래머스/0/181897. 리스트 자르기/README.md
+++ 프로그래머스/0/181897. 리스트 자르기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 리스트 자르기 - 181897 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181897) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181897?language=java) 
 
 ### 성능 요약
 
-메모리: 79.2 MB, 시간: 0.02 ms
+메모리: 76.3 MB, 시간: 0.02 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 24일 20:20:10
+2024년 04월 24일 20:30:36
 
 ### 문제 설명
 
--- 프로그래머스/0/181897. 리스트 자르기/리스트 자르기.java
+++ 프로그래머스/0/181897. 리스트 자르기/리스트 자르기.java
@@ -1,26 +1,36 @@
 class Solution {
     public int[] solution(int n, int[] slicer, int[] num_list) {
-        int[] answer = {};
+//         int[] answer = {};
         
-        if (n == 1) {
-            answer = new int[slicer[1] + 1];
-            for (int i = 0; i < answer.length; i++) { answer[i] = num_list[i]; }
-        } else if (n == 2) {
-            int idx = 0;
-            answer = new int[num_list.length - slicer[0]];
-            for (int i = slicer[0]; i < num_list.length; i++) { answer[idx++] = num_list[i]; }
-        } else if (n == 3) {
-            int idx = 0;
-            answer = new int[slicer[1] - slicer[0] + 1];
-            for (int i = slicer[0]; i <= slicer[1]; i++) { answer[idx++] = num_list[i]; }
-        } else {
-            int idx = 0;
-            int len = 0;
-            for (int i = slicer[0]; i <= slicer[1]; i += slicer[2]) { len++; }
-            answer = new int[len];
-            for (int i = slicer[0]; i <= slicer[1]; i += slicer[2]) { answer[idx++] = num_list[i]; }
+//         if (n == 1) {
+//             answer = new int[slicer[1] + 1];
+//             for (int i = 0; i < answer.length; i++) { answer[i] = num_list[i]; }
+//         } else if (n == 2) {
+//             int idx = 0;
+//             answer = new int[num_list.length - slicer[0]];
+//             for (int i = slicer[0]; i < num_list.length; i++) { answer[idx++] = num_list[i]; }
+//         } else if (n == 3) {
+//             int idx = 0;
+//             answer = new int[slicer[1] - slicer[0] + 1];
+//             for (int i = slicer[0]; i <= slicer[1]; i++) { answer[idx++] = num_list[i]; }
+//         } else {
+//             int idx = 0;
+//             int len = 0;
+//             for (int i = slicer[0]; i <= slicer[1]; i += slicer[2]) { len++; }
+//             answer = new int[len];
+//             for (int i = slicer[0]; i <= slicer[1]; i += slicer[2]) { answer[idx++] = num_list[i]; }
+//         }
+        
+//         return answer;
+        
+        int start = n == 1 ? 0 : slicer[0];
+        int end = n == 2 ? num_list.length - 1 : slicer[1];
+        int step = n == 4 ? slicer[2] : 1;
+        
+        int[] answer = new int[(end - start + step) / step];
+        for (int i = start, j = 0; i <= end; i += step) {
+            answer[j++] = num_list[i];
         }
-        
         return answer;
     }
 }
