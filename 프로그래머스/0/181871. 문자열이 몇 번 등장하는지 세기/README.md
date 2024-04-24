7cc2e53438769486ad87036d3197f429d5358b18
Greta Jiyun Lee <51826219+howecofe@users.noreply.github.com>
2024-04-24 13:15:31 +0900

    [level 0] Title: 문자열이 몇 번 등장하는지 세기, Time: 0.09 ms, Memory: 76.6 MB -BaekjoonHub

--- 프로그래머스/0/181871. 문자열이 몇 번 등장하는지 세기/README.md
+++ 프로그래머스/0/181871. 문자열이 몇 번 등장하는지 세기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 문자열이 몇 번 등장하는지 세기 - 181871 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181871) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181871?language=java) 
 
 ### 성능 요약
 
-메모리: 74.4 MB, 시간: 0.06 ms
+메모리: 76.6 MB, 시간: 0.09 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 24일 13:14:14
+2024년 04월 24일 13:15:29
 
 ### 문제 설명
 
--- 프로그래머스/0/181871. 문자열이 몇 번 등장하는지 세기/문자열이 몇 번 등장하는지 세기.java
+++ 프로그래머스/0/181871. 문자열이 몇 번 등장하는지 세기/문자열이 몇 번 등장하는지 세기.java
@@ -1,8 +1,11 @@
 class Solution {
     public int solution(String myString, String pat) {
         int cnt = 0;
-        for (int i = 0; i < myString.length() - pat.length() + 1; i++) {
-            if (myString.substring(i, i + pat.length()).equals(pat)) { cnt++; }
+        // for (int i = 0; i < myString.length() - pat.length() + 1; i++) {
+        //     if (myString.substring(i, i + pat.length()).equals(pat)) { cnt++; }
+        // }
+        for (int i = 0; i < myString.length(); i++) {
+            if (myString.substring(i).startsWith(pat)) { cnt++; }
         }
         return cnt;
     }
