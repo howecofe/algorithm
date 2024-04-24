8b97ef6be0b4061748f0e9676285222d520ae705
Greta Jiyun Lee <51826219+howecofe@users.noreply.github.com>
2024-04-24 13:31:47 +0900

    [level 0] Title: 문자열 뒤집기, Time: 9.65 ms, Memory: 73.4 MB -BaekjoonHub

--- 프로그래머스/0/181905. 문자열 뒤집기/README.md
+++ 프로그래머스/0/181905. 문자열 뒤집기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 문자열 뒤집기 - 181905 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181905) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181905?language=java) 
 
 ### 성능 요약
 
-메모리: 69.6 MB, 시간: 11.53 ms
+메모리: 73.4 MB, 시간: 9.65 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 24일 13:23:17
+2024년 04월 24일 13:31:45
 
 ### 문제 설명
 
--- 프로그래머스/0/181905. 문자열 뒤집기/문자열 뒤집기.java
+++ 프로그래머스/0/181905. 문자열 뒤집기/문자열 뒤집기.java
@@ -1,13 +1,16 @@
 class Solution {
     public String solution(String my_string, int s, int e) {
-        StringBuilder sb = new StringBuilder();
-        String s1 = "";
-        String s2 = "";
-        for (int i = 0; i < my_string.length(); i++) {
-            if (i < s) { s1 += my_string.charAt(i); }
-            else if (s <= i && i <= e) { sb.insert(0, my_string.charAt(i)); }
-            else { s2 += my_string.charAt(i); }
-        }
-        return s1 + sb.toString() + s2;
+        // StringBuilder sb = new StringBuilder();
+        // String s1 = "";
+        // String s2 = "";
+        // for (int i = 0; i < my_string.length(); i++) {
+        //     if (i < s) { s1 += my_string.charAt(i); }
+        //     else if (s <= i && i <= e) { sb.insert(0, my_string.charAt(i)); }
+        //     else { s2 += my_string.charAt(i); }
+        // }
+        // return s1 + sb.toString() + s2;
+        
+        StringBuilder sb = new StringBuilder(my_string.substring(s, e + 1));
+        return my_string.substring(0, s) + sb.reverse() + my_string.substring(e + 1);
     }
 }
