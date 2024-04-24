aefab80ad185f8bb649cd1d82d82110d1b6f5952
Greta Jiyun Lee <51826219+howecofe@users.noreply.github.com>
2024-04-24 13:10:18 +0900

    [level 0] Title: 간단한 논리 연산, Time: 0.02 ms, Memory: 69.2 MB -BaekjoonHub

--- 프로그래머스/0/181917. 간단한 논리 연산/README.md
+++ 프로그래머스/0/181917. 간단한 논리 연산/README.md
@@ -1,10 +1,10 @@
 # [level 0] 간단한 논리 연산 - 181917 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181917) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181917?language=java) 
 
 ### 성능 요약
 
-메모리: 70.5 MB, 시간: 0.01 ms
+메모리: 69.2 MB, 시간: 0.02 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 24일 13:07:55
+2024년 04월 24일 13:10:16
 
 ### 문제 설명
 
--- 프로그래머스/0/181917. 간단한 논리 연산/간단한 논리 연산.java
+++ 프로그래머스/0/181917. 간단한 논리 연산/간단한 논리 연산.java
@@ -1,10 +1,12 @@
 class Solution {
     public boolean solution(boolean x1, boolean x2, boolean x3, boolean x4) {
-        boolean b1 = true, b2 = true;
+//         boolean b1 = true, b2 = true;
         
-        if (!x1 && !x2) { b1 = false; }
-        if (!x3 && !x4) { b2 = false; }
+//         if (!x1 && !x2) { b1 = false; }
+//         if (!x3 && !x4) { b2 = false; }
         
-        return b1 && b2 ? true: false;
+//         return b1 && b2;
+        
+        return (x1 || x2) && (x3 || x4);
     }
 }
