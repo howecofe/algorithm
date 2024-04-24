b2d630c12b66e74508220ec30c418aba4f40e0a6
Greta Jiyun Lee <51826219+howecofe@users.noreply.github.com>
2024-04-24 23:53:49 +0900

    [level 0] Title: 왼쪽 오른쪽, Time: 0.03 ms, Memory: 71.4 MB -BaekjoonHub

--- 프로그래머스/0/181890. 왼쪽 오른쪽/README.md
+++ 프로그래머스/0/181890. 왼쪽 오른쪽/README.md
@@ -1,10 +1,10 @@
 # [level 0] 왼쪽 오른쪽 - 181890 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181890) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181890?language=java) 
 
 ### 성능 요약
 
-메모리: 72.8 MB, 시간: 1.38 ms
+메모리: 71.4 MB, 시간: 0.03 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 24일 23:43:11
+2024년 04월 24일 23:53:47
 
 ### 문제 설명
 
--- 프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.java
+++ 프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.java
@@ -1,24 +1,38 @@
+import java.util.*;
+
 class Solution {
     public String[] solution(String[] str_list) {
-        String str = "";
-        for (String s : str_list) { str += s; }
+//         String str = "";
+//         for (String s : str_list) { str += s; }
         
-        String result = "";
-        int li = str.indexOf("l");
-        int ri = str.indexOf("r");
+//         String result = "";
+//         int li = str.indexOf("l");
+//         int ri = str.indexOf("r");
         
-        if (li != -1 && ri != -1) {
-            if (0 < li && li < ri) { 
-                result = str.substring(0, li);
-            } else if (ri != str.length() - 1 && ri < li) { 
-                result = str.substring(ri + 1);
+//         if (li != -1 && ri != -1) {
+//             if (0 < li && li < ri) { 
+//                 result = str.substring(0, li);
+//             } else if (ri != str.length() - 1 && ri < li) { 
+//                 result = str.substring(ri + 1);
+//             }
+//         } else if (li != -1 && ri == -1) {
+//             if (0 < li) { result = str.substring(0, li); }
+//         } else if (li == -1 && ri != -1) {
+//             if (ri != str.length() - 1) { result = str.substring(ri + 1); }
+//         }
+        
+//         return result.isEmpty() ? new String[]{} : result.split("");
+        
+        for (int i = 0; i < str_list.length; i++) {
+            if (str_list[i].equals("l")) {
+                return Arrays.copyOfRange(str_list, 0, i);
+            } else if (str_list[i].equals("r")) {
+                return Arrays.copyOfRange(str_list, i + 1, str_list.length);
             }
-        } else if (li != -1 && ri == -1) {
-            if (0 < li) { result = str.substring(0, li); }
-        } else if (li == -1 && ri != -1) {
-            if (ri != str.length() - 1) { result = str.substring(ri + 1); }
         }
         
-        return result.isEmpty() ? new String[]{} : result.split("");
+        return new String[0]; // new String[]{};
     }
 }
+
+// [Note] Arrays.copyOfRange() 메서드는 범위가 유효하지 않은 경우 길이가 0인 빈 문자열 반환 (new String[0]와 동일)
