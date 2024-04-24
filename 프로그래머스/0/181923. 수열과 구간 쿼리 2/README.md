493d477cdf6785b437fae34594fd9052cbff876e
Greta Jiyun Lee <51826219+howecofe@users.noreply.github.com>
2024-04-24 12:19:47 +0900

    [level 0] Title: 수열과 구간 쿼리 2, Time: 2.45 ms, Memory: 68.6 MB -BaekjoonHub

--- 프로그래머스/0/181923. 수열과 구간 쿼리 2/README.md
+++ 프로그래머스/0/181923. 수열과 구간 쿼리 2/README.md
@@ -1,10 +1,10 @@
 # [level 0] 수열과 구간 쿼리 2 - 181923 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181923) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181923?language=java) 
 
 ### 성능 요약
 
-메모리: 73.5 MB, 시간: 2.38 ms
+메모리: 68.6 MB, 시간: 2.45 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 24일 12:15:37
+2024년 04월 24일 12:19:46
 
 ### 문제 설명
 
--- 프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.java
+++ 프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.java
@@ -3,7 +3,7 @@ class Solution {
         int[] answer = new int[queries.length];
         
         for (int i = 0 ; i < queries.length; i++) {
-            int min = 1000000;
+            int min = Integer.MAX_VALUE;
             boolean exists = false;
             for (int j = queries[i][0]; j <= queries[i][1]; j++) {                
                 if (queries[i][2] < arr[j] && min > arr[j]) {
