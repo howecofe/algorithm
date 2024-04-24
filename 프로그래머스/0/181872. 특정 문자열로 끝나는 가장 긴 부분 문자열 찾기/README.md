1d7312b96f0388687df0c6c06e3f578d712581c3
Greta Jiyun Lee <51826219+howecofe@users.noreply.github.com>
2024-04-24 19:19:03 +0900

    [level 0] Title: 특정 문자열로 끝나는 가장 긴 부분 문자열 찾기, Time: 1.69 ms, Memory: 70.2 MB -BaekjoonHub

--- 프로그래머스/0/181872. 특정 문자열로 끝나는 가장 긴 부분 문자열 찾기/README.md
+++ 프로그래머스/0/181872. 특정 문자열로 끝나는 가장 긴 부분 문자열 찾기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 특정 문자열로 끝나는 가장 긴 부분 문자열 찾기 - 181872 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181872) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181872?language=java) 
 
 ### 성능 요약
 
-메모리: 78.7 MB, 시간: 0.02 ms
+메모리: 70.2 MB, 시간: 1.69 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 24일 19:17:45
+2024년 04월 24일 19:19:02
 
 ### 문제 설명
 
--- 프로그래머스/0/181872. 특정 문자열로 끝나는 가장 긴 부분 문자열 찾기/특정 문자열로 끝나는 가장 긴 부분 문자열 찾기.java
+++ 프로그래머스/0/181872. 특정 문자열로 끝나는 가장 긴 부분 문자열 찾기/특정 문자열로 끝나는 가장 긴 부분 문자열 찾기.java
@@ -1,13 +1,15 @@
 class Solution {
     public String solution(String myString, String pat) {
-        String answer = "";
+//         String answer = "";
         
-        for (int i = myString.length() - 1; i >= 0; i--) {
-            if (myString.substring(0, i + 1).endsWith(pat)) { 
-                answer = myString.substring(0, i + 1);
-                break;
-            }
-        }
-        return answer;
+//         for (int i = myString.length() - 1; i >= 0; i--) {
+//             if (myString.substring(0, i + 1).endsWith(pat)) { 
+//                 answer = myString.substring(0, i + 1);
+//                 break;
+//             }
+//         }
+//         return answer;
+        
+        return myString.substring(0, myString.lastIndexOf(pat)) + pat;
     }
 }
