37e242081eebe36146f89d3e907bd7359469b21c
Greta Jiyun Lee <51826219+howecofe@users.noreply.github.com>
2024-04-24 16:31:32 +0900

    [level 0] Title: 순서 바꾸기, Time: 0.03 ms, Memory: 82.2 MB -BaekjoonHub

--- 프로그래머스/0/181891. 순서 바꾸기/README.md
+++ 프로그래머스/0/181891. 순서 바꾸기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 순서 바꾸기 - 181891 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181891) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181891?language=java) 
 
 ### 성능 요약
 
-메모리: 73.6 MB, 시간: 0.02 ms
+메모리: 82.2 MB, 시간: 0.03 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 24일 16:28:30
+2024년 04월 24일 16:31:30
 
 ### 문제 설명
 
--- 프로그래머스/0/181891. 순서 바꾸기/순서 바꾸기.java
+++ 프로그래머스/0/181891. 순서 바꾸기/순서 바꾸기.java
@@ -1,9 +1,12 @@
 class Solution {
     public int[] solution(int[] num_list, int n) {
         int[] answer = new int[num_list.length];
-        for (int i = 0; i < answer.length; i++) {
-            if (i <= num_list.length-n-1) { answer[i] = num_list[i + n]; }
-            else { answer[i] = num_list[i + n - num_list.length]; }
+        int idx = 0;
+        for (int i = n; i < num_list.length; i++) {
+            answer[idx++] = num_list[i];
+        }
+        for(int i = 0; i < n; i++) {
+            answer[idx++] = num_list[i];
         }
         
         return answer;
