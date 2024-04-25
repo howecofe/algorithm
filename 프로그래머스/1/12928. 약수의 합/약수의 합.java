71432fa426773f870081c8e4cb05f7ca71aee49d
Greta Jiyun Lee <51826219+howecofe@users.noreply.github.com>
2024-04-26 01:17:11 +0900

    [level 1] Title: 약수의 합, Time: 0.04 ms, Memory: 77.3 MB -BaekjoonHub

--- 프로그래머스/1/12928. 약수의 합/약수의 합.java
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.java
@@ -2,12 +2,16 @@ class Solution {
     public int solution(int n) {
         int answer = 0;
         
-        int i = 1;
-        while (i <= n) {            
+        // int i = 1;
+        // while (i <= n) {            
+        //     if (n % i == 0) { answer += i; }
+        //     i++;
+        // }
+
+        for (int i = 1; i <= n / 2; i++) {
             if (n % i == 0) { answer += i; }
-            i++;
         }
         
-        return answer;
+        return answer + n;
     }
 }
