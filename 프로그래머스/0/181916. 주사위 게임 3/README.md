b8aebc739fa2585108cb6e06a5feff63f42853d3
Greta Jiyun Lee <51826219+howecofe@users.noreply.github.com>
2024-04-25 21:00:07 +0900

    [level 0] Title: 주사위 게임 3, Time: 0.49 ms, Memory: 77.5 MB -BaekjoonHub

--- 프로그래머스/0/181916. 주사위 게임 3/README.md
+++ 프로그래머스/0/181916. 주사위 게임 3/README.md
@@ -1,10 +1,10 @@
 # [level 0] 주사위 게임 3 - 181916 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181916) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181916?language=java) 
 
 ### 성능 요약
 
-메모리: 72.8 MB, 시간: 0.02 ms
+메모리: 77.5 MB, 시간: 0.49 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 25일 20:44:51
+2024년 04월 25일 21:00:05
 
 ### 문제 설명
 
--- 프로그래머스/0/181916. 주사위 게임 3/주사위 게임 3.java
+++ 프로그래머스/0/181916. 주사위 게임 3/주사위 게임 3.java
@@ -1,41 +1,67 @@
-class Solution {
-    public int solution(int a, int b, int c, int d) {        
-        int[] cnt = new int[7];
-        cnt[a]++;
-        cnt[b]++;
-        cnt[c]++;
-        cnt[d]++;
-        
-        int count = 0;
-        for (int i = 1; i < cnt.length; i++) {
-            if (cnt[i] == 1) { count++; }
-            
-            if (cnt[i] == 4) { return 1111 * i; }
-            
-            if (cnt[i] == 3) {
-                for (int j = 1; j < cnt.length; j++) {
-                    if (cnt[j] == 1) { return (10 * i + j) * (10 * i + j); }
-                }
-            }
-            
-            if (cnt[i] == 2) {
-                for (int j = 1; j < cnt.length; j++) {
-                    if (i != j) {
-                        if (cnt[j] == 1) {
-                            for (int k = 1; k < cnt.length; k++) {
-                                if (j != k && cnt[k] == 1) { return j * k; }
-                            }
-                        }
+import java.util.*;
 
-                        if (cnt[j] == 2) { return (i + j) * Math.abs(i - j); }
-                    }
+class Solution {
+    public int solution(int a, int b, int c, int d) {
+        // 1. 카운트 배열을 이용한 풀이
+//         int[] cnt = new int[7];
+//         cnt[a]++;
+//         cnt[b]++;
+//         cnt[c]++;
+//         cnt[d]++;
+        
+//         int count = 0;
+//         for (int i = 1; i < cnt.length; i++) {
+//             if (cnt[i] == 1) { count++; }
+            
+//             if (cnt[i] == 4) { return 1111 * i; }
+            
+//             if (cnt[i] == 3) {
+//                 for (int j = 1; j < cnt.length; j++) {
+//                     if (cnt[j] == 1) { return (10 * i + j) * (10 * i + j); }
+//                 }
+//             }
+            
+//             if (cnt[i] == 2) {
+//                 for (int j = 1; j < cnt.length; j++) {
+//                     if (i != j) {
+//                         if (cnt[j] == 1) {
+//                             for (int k = 1; k < cnt.length; k++) {
+//                                 if (j != k && cnt[k] == 1) { return j * k; }
+//                             }
+//                         }
+
+//                         if (cnt[j] == 2) { return (i + j) * Math.abs(i - j); }
+//                     }
                     
-                }
-            }
+//                 }
+//             }
+//         }
+        
+//         if (count == 4) { return Math.min(Math.min(a, b), Math.min(c, d)); }
+        
+//         return -1;
+        
+        // 2. 정렬을 이용한 풀이
+        int answer = 0;
+        int[] dice = {a, b, c, d};
+        Arrays.sort(dice);
+        
+        if (dice[0] == dice[3]) { 
+            answer = dice[0] * 1111;
+        } else if (dice[0] == dice[2] || dice[1] == dice[3]) {
+            answer = (int) Math.pow(10 * dice[1] + (dice[0] + dice[3] - dice[1]), 2);
+        } else if (dice[0] == dice[1] && dice[2] == dice[3]) {
+            answer = (dice[0] + dice[3]) * (dice[3] - dice[0]);
+        } else if (dice[0] == dice[1]) {
+            answer = dice[2] * dice[3];
+        } else if (dice[1] == dice[2]) {
+            answer = dice[0] * dice[3];
+        } else if (dice[2] == dice[3]) {
+            answer = dice[0] * dice[1];
+        } else {
+            answer = dice[0];
         }
         
-        if (count == 4) { return Math.min(Math.min(a, b), Math.min(c, d)); }
-        
-        return -1;
+        return answer;
     }
 }
