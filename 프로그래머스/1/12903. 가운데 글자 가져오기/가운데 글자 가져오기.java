ffe812695052de3ec244936bb1c300e767fbd2c7
Greta Jiyun Lee <51826219+howecofe@users.noreply.github.com>
2024-04-26 16:50:14 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.03 ms, Memory: 76.9 MB -BaekjoonHub

--- 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.java
+++ 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.java
@@ -1,13 +1,6 @@
 class Solution {
     public String solution(String s) {
-        String answer = "";
-        
-        if (s.length() % 2 == 1) { answer += s.charAt(s.length() / 2); }
-        else {
-            answer += s.charAt(s.length() / 2 - 1);
-            answer += s.charAt(s.length() / 2);
-        }
-        
-        return answer;
+        if (s.length() % 2 == 1) { return s.substring(s.length() / 2, s.length() / 2 + 1); }
+        else { return s.substring(s.length() / 2 - 1, s.length() / 2 + 1); }
     }
 }
