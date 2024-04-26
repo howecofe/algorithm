510aed6b97f82e4242b46f72660ce16fbccabf10
Greta Jiyun Lee <51826219+howecofe@users.noreply.github.com>
2024-04-26 15:53:48 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 7.42 ms, Memory: 78.4 MB -BaekjoonHub

--- 프로그래머스/1/12919. 서울에서 김서방 찾기/README.md
+++ 프로그래머스/1/12919. 서울에서 김서방 찾기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 서울에서 김서방 찾기 - 12919 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12919) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12919?language=java) 
 
 ### 성능 요약
 
-메모리: 78.8 MB, 시간: 7.87 ms
+메모리: 78.4 MB, 시간: 7.42 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 26일 15:52:24
+2024년 04월 26일 15:53:46
 
 ### 문제 설명
 
--- 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
+++ 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
@@ -1,9 +1,12 @@
+import java.util.*;
+
 class Solution {
     public String solution(String[] seoul) {
-        int x = 0;
-        for (int i = 0; i < seoul.length; i++) {
-            if (seoul[i].equals("Kim")) { x = i; }
-        }
+        // int x = 0;
+        // for (int i = 0; i < seoul.length; i++) {
+        //     if (seoul[i].equals("Kim")) { x = i; }
+        // }
+        int x = Arrays.asList(seoul).indexOf("Kim");
         return "김서방은 " + x + "에 있다";
     }
 }
