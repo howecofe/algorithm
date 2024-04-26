745fd820d04b2859dcc09d48f3d76e2f3c8d59cd
Greta Jiyun Lee <51826219+howecofe@users.noreply.github.com>
2024-04-26 20:26:32 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 47.32 ms, Memory: 117 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -3,9 +3,8 @@ import java.util.*;
 public class Solution {
     public int[] solution(int[] arr) {
         List<Integer> list = new ArrayList<>();
-        int tmp = arr[0];
-        list.add(tmp);
-        for (int i = 1; i < arr.length ; i++) {
+        int tmp = -1;
+        for (int i = 0; i < arr.length ; i++) {
             if (tmp != arr[i]) {
                 list.add(arr[i]);
                 tmp = arr[i];
