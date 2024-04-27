9557570099a3d7f2fa46fb2afc69b4054eeb8cb7
Greta Jiyun Lee <51826219+howecofe@users.noreply.github.com>
2024-04-28 00:41:00 +0900

    [level 1] Title: 예산, Time: 0.51 ms, Memory: 75.6 MB -BaekjoonHub

--- 프로그래머스/1/12982. 예산/README.md
+++ 프로그래머스/1/12982. 예산/README.md
@@ -1,10 +1,10 @@
 # [level 1] 예산 - 12982 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12982#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12982?language=java) 
 
 ### 성능 요약
 
-메모리: 77.4 MB, 시간: 0.34 ms
+메모리: 75.6 MB, 시간: 0.51 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 28일 00:39:15
+2024년 04월 28일 00:40:58
 
 ### 문제 설명
 
--- 프로그래머스/1/12982. 예산/예산.java
+++ 프로그래머스/1/12982. 예산/예산.java
@@ -2,15 +2,28 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] d, int budget) {
+//         Arrays.sort(d);
+//         int sum = 0;
+//         int answer = d.length;
+        
+//         for (int i = 0; i < d.length; i++) {
+//             sum += d[i];
+            
+//             if (sum == budget) { answer = i + 1; break; }
+//             else if (sum > budget) { answer = i; break; }
+//         }
+        
+//         return answer;
+        
         Arrays.sort(d);
-        int sum = 0;
-        int answer = d.length;
+        int answer = 0;
         
         for (int i = 0; i < d.length; i++) {
-            sum += d[i];
+            budget -= d[i];
             
-            if (sum == budget) { answer = i + 1; break; }
-            else if (sum > budget) { answer = i; break; }
+            if (budget < 0) { break; }
+            
+            answer++;
         }
         
         return answer;
