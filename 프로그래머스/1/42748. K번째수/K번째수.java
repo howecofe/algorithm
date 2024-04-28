731f2d852ba9602141f034fe196ebaa25eff8f97
Greta Jiyun Lee <51826219+howecofe@users.noreply.github.com>
2024-04-28 16:45:26 +0900

    [level 1] Title: K번째수, Time: 0.27 ms, Memory: 76 MB -BaekjoonHub

---
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
+        
+        for (int i = 0; i < answer.length; i++) {
+            List<Integer> list = new ArrayList<>();
+            for (int j = commands[i][0] - 1; j < commands[i][1]; j++) { list.add(array[j]); }
+            list.sort(Comparator.naturalOrder());
+            answer[i] = list.get(commands[i][2] - 1);
+        }
+        return answer;
+    }
+}
