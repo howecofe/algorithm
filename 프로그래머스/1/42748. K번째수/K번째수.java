8906d6eb2c0f9b7ce66f756246f9f38dee67d563
Greta Jiyun Lee <51826219+howecofe@users.noreply.github.com>
2024-04-28 16:47:47 +0900

    [level 1] Title: K번째수, Time: 0.36 ms, Memory: 72.1 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -5,10 +5,14 @@ class Solution {
         int[] answer = new int[commands.length];
         
         for (int i = 0; i < answer.length; i++) {
-            List<Integer> list = new ArrayList<>();
-            for (int j = commands[i][0] - 1; j < commands[i][1]; j++) { list.add(array[j]); }
-            list.sort(Comparator.naturalOrder());
-            answer[i] = list.get(commands[i][2] - 1);
+            // List<Integer> list = new ArrayList<>();
+            // for (int j = commands[i][0] - 1; j < commands[i][1]; j++) { list.add(array[j]); }
+            // list.sort(Comparator.naturalOrder());
+            // answer[i] = list.get(commands[i][2] - 1);
+            
+            int[] arr = Arrays.copyOfRange(array, commands[i][0] - 1, commands[i][1]);
+            Arrays.sort(arr);
+            answer[i] = arr[commands[i][2] - 1];
         }
         return answer;
     }
--- 프로그래머스/1/42748. K번째수/README.md
+++ 프로그래머스/1/42748. K번째수/README.md
@@ -1,10 +1,10 @@
 # [level 1] K번째수 - 42748 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42748) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42748?language=java) 
 
 ### 성능 요약
 
-메모리: 76 MB, 시간: 0.27 ms
+메모리: 72.1 MB, 시간: 0.36 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 28일 16:45:25
+2024년 04월 28일 16:47:46
 
 ### 문제 설명
 
