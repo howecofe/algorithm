395e13e3e3d4f4ed3aeb65f9fefa08c03a6d3bf1
Greta Jiyun Lee <51826219+howecofe@users.noreply.github.com>
2024-04-28 10:50:26 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 9.29 ms, Memory: 75.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String s) {
+        Map<String, String> map = new HashMap<>();
+        map.put("zero", "0");
+        map.put("one", "1");
+        map.put("two", "2");
+        map.put("three", "3");
+        map.put("four", "4");
+        map.put("five", "5");
+        map.put("six", "6");
+        map.put("seven", "7");
+        map.put("eight", "8");
+        map.put("nine", "9");
+
+        String word = "";
+        String numStr = "";
+        for (char c : s.toCharArray()) {
+            if ('0' <= c && c <= '9') { word += c; }
+            else {
+                numStr += c;
+                
+                for (String k : map.keySet()) {
+                    if (k.equals(numStr)) { 
+                        word += map.get(k);
+                        numStr = "";
+                        break;
+                    }
+                }
+            }
+        }
+        
+        return Integer.parseInt(word);
+    }
+}
