0db38140453ccfa3b829853e800a832dd6e2ce0e
Greta Jiyun Lee <51826219+howecofe@users.noreply.github.com>
2024-04-28 22:19:23 +0900

    [level 1] Title: 폰켓몬, Time: 5.26 ms, Memory: 79.9 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/README.md
+++ 프로그래머스/1/1845. 폰켓몬/README.md
@@ -1,10 +1,10 @@
 # [level 1] 폰켓몬 - 1845 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1845) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1845?language=java) 
 
 ### 성능 요약
 
-메모리: 77.4 MB, 시간: 2.44 ms
+메모리: 79.9 MB, 시간: 5.26 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 28일 22:16:06
+2024년 04월 28일 22:19:21
 
 ### 문제 설명
 
--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -5,7 +5,8 @@ class Solution {
         Set<Integer> set = new HashSet();
         for (int n : nums) { set.add(n); }
         
-        if (set.size() <= nums.length / 2) { return set.size(); }
-        else { return nums.length / 2; }
+        // if (set.size() <= nums.length / 2) { return set.size(); }
+        // else { return nums.length / 2; }
+        return Math.min(set.size(), nums.length / 2);
     }
 }
