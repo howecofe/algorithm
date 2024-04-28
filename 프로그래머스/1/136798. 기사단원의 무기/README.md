ce0ee72e3868aac0f8021350fe71403851a03064
Greta Jiyun Lee <51826219+howecofe@users.noreply.github.com>
2024-04-29 00:52:59 +0900

    [level 1] Title: 기사단원의 무기, Time: 0.72 ms, Memory: 74.3 MB -BaekjoonHub

--- 프로그래머스/1/136798. 기사단원의 무기/README.md
+++ 프로그래머스/1/136798. 기사단원의 무기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 기사단원의 무기 - 136798 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/136798) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/136798?language=java) 
 
 ### 성능 요약
 
-메모리: 76.3 MB, 시간: 2.62 ms
+메모리: 74.3 MB, 시간: 0.72 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 29일 00:44:15
+2024년 04월 29일 00:52:55
 
 ### 문제 설명
 
--- 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.java
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.java
@@ -3,9 +3,10 @@ class Solution {
         int answer = 0;
         
         for (int i = 1; i <= number; i++) {
-            int cnt = 1;
-            for (int j = 1; j <= i / 2; j++) {
-                if (i % j == 0) { cnt++; }
+            int cnt = 0;
+            for (int j = 1; j <= Math.sqrt(i); j++) {
+                if (j == Math.sqrt(i)) { cnt++; } // 제곱근으로 나누어 떨어지면 +1 카운트
+                else if (i % j == 0) { cnt += 2; } // 제곱근이 아닌 수로 나누어 떨어지면 +2 카운트
             }
             answer += cnt > limit ? power : cnt;
         }
