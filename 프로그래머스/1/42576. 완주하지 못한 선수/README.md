ed7bcfb0ae81d253fda1b703a756b96116194e48
Greta Jiyun Lee <51826219+howecofe@users.noreply.github.com>
2024-04-30 00:14:40 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 361.30 ms, Memory: 97 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/README.md
+++ 프로그래머스/1/42576. 완주하지 못한 선수/README.md
@@ -1,10 +1,10 @@
 # [level 1] 완주하지 못한 선수 - 42576 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576?language=java) 
 
 ### 성능 요약
 
-메모리: 95.6 MB, 시간: 314.07 ms
+메모리: 97 MB, 시간: 361.30 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 30일 00:08:59
+2024년 04월 30일 00:14:38
 
 ### 문제 설명
 
--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -2,21 +2,21 @@ import java.util.*;
 
 class Solution {
     public String solution(String[] participant, String[] completion) {
-        String answer = "";
-        
-        if (completion.length == 0) { return participant[0]; }
+        // Note. i를 전역변수로 선언 후, for문 내에서도 사용한다.
+        int i = 0; 
+        // if (completion.length == 0) { return participant[0]; }
         
         Arrays.sort(participant);
         Arrays.sort(completion);
         
-        if (!completion[completion.length - 1].equals(participant[participant.length - 1])) {
-            return participant[participant.length - 1];
+        // if (!completion[completion.length - 1].equals(participant[participant.length - 1])) {
+        //     return participant[participant.length - 1];
+        // }
+        
+        for (i = 0; i < completion.length; i++) {
+            if (!participant[i].equals(completion[i])) { return participant[i]; }
         }
         
-        for (int i = 0; i < completion.length; i++) {
-            if (!participant[i].equals(completion[i])) { answer = participant[i]; break; }
-        }
-        
-        return answer;
+        return participant[i];
     }
 }
