1c709e7c04f01891c67c21ed0fe8edca53d3cb33
Greta Jiyun Lee <51826219+howecofe@users.noreply.github.com>
2024-05-01 18:20:06 +0900

    [level 1] Title: 햄버거 만들기, Time: 0.03 ms, Memory: 75 MB -BaekjoonHub

--- 프로그래머스/1/133502. 햄버거 만들기/README.md
+++ 프로그래머스/1/133502. 햄버거 만들기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 햄버거 만들기 - 133502 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/133502) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/133502?language=java) 
 
 ### 성능 요약
 
-메모리: 78.6 MB, 시간: 0.07 ms
+메모리: 75 MB, 시간: 0.03 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 05월 01일 18:00:07
+2024년 05월 01일 18:20:05
 
 ### 문제 설명
 
--- 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.java
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.java
@@ -1,17 +1,36 @@
-import java.util.*;
-
 class Solution {
     public int solution(int[] ingredient) {
+        // 1. StringBuilder 사용
+//         int answer = 0;
+//         StringBuilder sb = new StringBuilder();
+        
+//         for (int ing : ingredient) {
+//             sb.append(ing);
+//             if (sb.length() >= 4 && sb.substring(sb.length() - 4).equals("1231")) {
+//                 sb.delete(sb.length() - 4, sb.length());
+//                 answer++;
+//             }
+//         }
+//         return answer;
+        
+        // 2. Stack 배열 사용
         int answer = 0;
-        StringBuilder sb = new StringBuilder();
+        int[] stack = new int[ingredient.length];
+        int idx = 0;
         
         for (int ing : ingredient) {
-            sb.append(ing);
-            if (sb.length() >= 4 && sb.substring(sb.length() - 4).equals("1231")) {
-                sb.delete(sb.length() - 4, sb.length());
+            stack[idx++] = ing;
+            
+            if (idx >= 4
+                && stack[idx - 1] == 1 
+                && stack[idx - 2] == 3 
+                && stack[idx - 3] == 2 
+                && stack[idx - 4] == 1) {
                 answer++;
+                idx -= 4;
             }
         }
+        
         return answer;
     }
 }
