a6a179d170372a7aac41997eca234a3c37f0cc30
Greta Jiyun Lee <51826219+howecofe@users.noreply.github.com>
2024-05-02 10:52:47 +0900

    [level 1] Title: 바탕화면 정리, Time: 0.14 ms, Memory: 72.4 MB -BaekjoonHub

--- 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.java
+++ 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.java
@@ -1,24 +1,19 @@
-import java.util.*;
-
 class Solution {
     public int[] solution(String[] wallpaper) {
-        List<int[]> points = new ArrayList<>();
+        int xmin = Integer.MAX_VALUE;
+        int ymin = Integer.MAX_VALUE;
+        int xmax = Integer.MIN_VALUE;
+        int ymax = Integer.MIN_VALUE;
         
         for (int i = 0; i < wallpaper.length; i++) {
             for (int j = 0; j < wallpaper[i].length(); j++) {
-                if (wallpaper[i].charAt(j) == '#') { points.add(new int[]{i, j}); }
+                if (wallpaper[i].charAt(j) == '#') {
+                    xmin = Math.min(xmin, i);
+                    xmax = Math.max(xmax, i);
+                    ymin = Math.min(ymin, j);
+                    ymax = Math.max(ymax, j);
+                }
             }
-        }
-        
-        int xmin = points.get(0)[0];
-        int xmax = points.get(points.size() - 1)[0];
-        
-        int ymin = points.get(0)[1];
-        int ymax = points.get(0)[1];
-        
-        for (int[] p : points) {
-            if (p[1] < ymin) { ymin = p[1]; }
-            if (p[1] > ymax) { ymax = p[1]; }
         }
         
         return new int[]{xmin, ymin, xmax + 1, ymax + 1};
