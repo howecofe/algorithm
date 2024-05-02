8763e7c71af1e3acea4123b1607b5a09d8a0d245
Greta Jiyun Lee <51826219+howecofe@users.noreply.github.com>
2024-05-02 12:44:17 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 1.85 ms, Memory: 73.3 MB -BaekjoonHub

--- 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
@@ -8,6 +8,8 @@ class Solution {
         int month = Integer.parseInt(today.split("\\.")[1]);
         int day = Integer.parseInt(today.split("\\.")[2]);
         
+        int currDays = getDays(year, month, day);
+        
         Map<String, Integer> termsMap = new HashMap<>();
         for (String t : terms) {
             termsMap.put(t.split(" ")[0], Integer.parseInt(t.split(" ")[1]));
@@ -17,36 +19,22 @@ class Solution {
             String[] date = privacies[i].split(" ")[0].split("\\.");
             String t = privacies[i].split(" ")[1];
             
-            int validYear = Integer.parseInt(date[0]) + (Integer.parseInt(date[1]) + termsMap.get(t) - 1) / 12;
-            int validMonth = ((Integer.parseInt(date[1]) + termsMap.get(t)) % 12 == 0) ? 12 : ((Integer.parseInt(date[1]) + termsMap.get(t)) % 12);
-            int validDay = Integer.parseInt(date[2]) - 1;
+            // 일수로 비교하기
+            int yyyy = Integer.parseInt(date[0]);
+            int mm = Integer.parseInt(date[1]) + termsMap.get(t);
+            int dd = Integer.parseInt(date[2]);
+            int validDays = getDays(yyyy, mm, dd) - 1;
             
-            if (validDay == 0) {
-                validDay = 28;
-                if (validMonth == 1) {
-                    validMonth = 12;
-                    validYear -= 1;
-                } else {
-                    validMonth -= 1;
-                }
-            }
-            
-            if (validYear > year) { continue; }
-            else if (validYear == year) {
-                if (validMonth > month) { continue; }
-                else if (validMonth == month) {
-                    if (validDay >= day) { continue; }
-                    else { list.add(i + 1); }
-                }
-                else { list.add(i + 1); }
-            } else {
-                list.add(i + 1);
-            }
+            if (validDays < currDays) { list.add(i + 1); }
         }
         
         int[] answer = new int[list.size()];
         for (int i = 0; i < answer.length; i++) { answer[i] = list.get(i); }
         
         return answer;
+    }
+    
+    private int getDays(int year, int month, int day) {
+        return year * 12 * 28 + month * 28 + day;
     }
 }
