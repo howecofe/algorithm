2b5cc681d60dba4c77f6b0336a87468cebd07731
Greta Jiyun Lee <51826219+howecofe@users.noreply.github.com>
2024-05-04 01:21:20 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 1.35 ms, Memory: 72 MB -BaekjoonHub

---
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
@@ -0,0 +1,22 @@
+class Solution {
+    public String solution(String s) {
+        String[] strArr = s.split(" ", -1);
+        
+        for (int i = 0; i < strArr.length; i++) {
+            String word = "";
+            
+            if (!strArr[i].isEmpty()) {
+                if (!('0' <= strArr[i].charAt(0) && strArr[i].charAt(0) <= '9')) {
+                    word += strArr[i].substring(0, 1).toUpperCase();
+                } else {
+                    word += strArr[i].substring(0, 1);
+                }
+                word += strArr[i].substring(1, strArr[i].length()).toLowerCase();
+            }
+            
+            strArr[i] = word;
+        }
+        
+        return String.join(" ", strArr);
+    }
+}
