88999ab1464added8cdb9ad45cf4d6d4a13e38bb
Greta Jiyun Lee <51826219+howecofe@users.noreply.github.com>
2024-05-12 23:12:12 +0900

    [level 2] Title: 전화번호 목록, Time: 328.64 ms, Memory: 136 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/README.md
+++ 프로그래머스/2/42577. 전화번호 목록/README.md
@@ -1,10 +1,10 @@
 # [level 2] 전화번호 목록 - 42577 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42577) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42577?language=java) 
 
 ### 성능 요약
 
-메모리: 96 MB, 시간: 281.91 ms
+메모리: 136 MB, 시간: 328.64 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 05월 12일 23:00:48
+2024년 05월 12일 23:12:10
 
 ### 문제 설명
 
--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -2,18 +2,33 @@ import java.util.*;
 
 class Solution {
     public boolean solution(String[] phone_book) {
-        Arrays.sort(phone_book);
+        // 1. 문자열 정렬 풀이
+//         Arrays.sort(phone_book);
         
-        for (int i = 0; i < phone_book.length - 1; i++) {
-            // if (phone_book[i].equals(phone_book[i + 1].substring(0, phone_book[i].length()))) {
-            //     return false;
-            // }
-            if (phone_book[i + 1].startsWith(phone_book[i])) {
-                return false;
-            }
+//         for (int i = 0; i < phone_book.length - 1; i++) {
+//             // if (phone_book[i].equals(phone_book[i + 1].substring(0, phone_book[i].length()))) {
+//             //     return false;
+//             // }
+//             if (phone_book[i + 1].startsWith(phone_book[i])) {
+//                 return false;
+//             }
+//         }
+        
+//         return true;
+        
+        // 2. 해시맵 풀이
+        Map<String, Integer> map = new HashMap<>();
+        for (int i = 0; i < phone_book.length; i++) {
+            map.put(phone_book[i], i);
         }
         
-        
+        for (String hp : phone_book) {
+            for (int i = 1; i < hp.length(); i++) { // 같은 번호가 중복해서 들어있지 않으므로 i 범위에서 hp.length()는 제외 (자기 자신 제외)
+                if (map.containsKey(hp.substring(0, i))) {
+                    return false;
+                }
+            }
+        }
         
         return true;
     }
