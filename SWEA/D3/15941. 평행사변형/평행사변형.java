5522fce325524950f29736b8f4cc3c7f4b2942a7
Greta Jiyun Lee <51826219+howecofe@users.noreply.github.com>
2024-05-18 00:07:28 +0900

    [D3] Title: 평행사변형, Time: 164 ms, Memory: 30,460 KB -BaekjoonHub

---
+++ SWEA/D3/15941. 평행사변형/평행사변형.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+import java.io.FileInputStream;
+
+public class Solution {
+    public static void main(String args[]) throws Exception
+    {
+        //System.setIn(new FileInputStream("res/input.txt"));
+
+        Scanner sc = new Scanner(System.in);
+        int T;
+        T=sc.nextInt();
+
+        for (int test_case = 1; test_case <= T; test_case++)
+        {
+            int n = sc.nextInt();
+            int answer = (int) Math.pow(n, 2);
+
+            System.out.printf("#%d %d\n", test_case, answer);
+        }
+    }
+}
