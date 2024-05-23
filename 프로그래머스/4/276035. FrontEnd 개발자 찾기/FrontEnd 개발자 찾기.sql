86b300981d1219b35d9091300a41b6cd3a9b6060
Greta Jiyun Lee <51826219+howecofe@users.noreply.github.com>
2024-05-23 09:05:04 +0900

    [level 4] Title: FrontEnd 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/4/276035. FrontEnd 개발자 찾기/FrontEnd 개발자 찾기.sql
@@ -0,0 +1,20 @@
+SELECT
+    ID,
+    EMAIL,
+    FIRST_NAME,
+    LAST_NAME
+FROM
+    DEVELOPERS
+WHERE
+    ID IN (
+        SELECT
+            DISTINCT A.ID
+        FROM
+            DEVELOPERS AS A,
+            SKILLCODES AS B
+        WHERE 
+            A.SKILL_CODE & B.CODE != 0
+            AND B.CATEGORY = 'Front End'
+    )
+ORDER BY
+    ID;
