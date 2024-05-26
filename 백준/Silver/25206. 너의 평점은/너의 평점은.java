f9930c4b36f5941e3af66fe964bf37d49ee6e750
Greta Jiyun Lee <51826219+howecofe@users.noreply.github.com>
2024-05-26 18:38:28 +0900

    [Silver V] Title: 너의 평점은, Time: 240 ms, Memory: 18236 KB -BaekjoonHub

---
+++ 백준/Silver/25206. 너의 평점은/너의 평점은.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double creditSum = 0;
+        double scoreSum = 0;
+        Map<String, Double> map = new HashMap<>();
+        map.put("A+", 4.5);
+        map.put("A0", 4.0);
+        map.put("B+", 3.5);
+        map.put("B0", 3.0);
+        map.put("C+", 2.5);
+        map.put("C0", 2.0);
+        map.put("D+", 1.5);
+        map.put("D0", 1.0);
+        map.put("F", 0.0);
+
+        for (int i = 0; i < 20; i++) {
+            sc.next();
+            double credit = sc.nextDouble();
+            String grade = sc.next();
+
+            if (grade.equals("P")) continue;
+
+            creditSum += credit;
+            scoreSum += credit * map.get(grade);
+        }
+
+        System.out.printf("%.6f", scoreSum / creditSum);
+    }
+}
