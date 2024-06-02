6fd52854a8ff4e97fd177845a7aac248a9e912e7
Greta Jiyun Lee <51826219+howecofe@users.noreply.github.com>
2024-06-02 22:49:48 +0900

    [Bronze II] Title: 벌집, Time: 216 ms, Memory: 17664 KB -BaekjoonHub

---
+++ 백준/Bronze/2292. 벌집/벌집.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int sum = 1;
+        int cnt = 0;
+
+        while (sum < n) {
+            cnt++;
+            sum += 6 * cnt;
+        }
+
+        System.out.println(cnt + 1);
+    }
+}
