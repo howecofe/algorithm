bf5c017a15f9382ffb5f0e8b15b175e668f33343
Greta Jiyun Lee <51826219+howecofe@users.noreply.github.com>
2024-06-03 23:54:23 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 12.48 ms, Memory: 91.9 MB -BaekjoonHub

--- 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
@@ -5,9 +5,7 @@ class Solution {
         String[] strArr = s.split(" ");
         int[] intArr = new int[strArr.length];
         
-        for (int i = 0; i < intArr.length; i++) {
-            intArr[i] = Integer.parseInt(strArr[i]);
-        }
+        for (int i = 0; i < intArr.length; i++) intArr[i] = Integer.parseInt(strArr[i]);
         
         Arrays.sort(intArr);
         
