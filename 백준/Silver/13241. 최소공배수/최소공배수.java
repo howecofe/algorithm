484c7e6f6b0cd20576d2324a313360233d446535
Greta Jiyun Lee <51826219+howecofe@users.noreply.github.com>
2024-06-11 21:49:55 +0900

    [Silver V] Title: 최소공배수, Time: 112 ms, Memory: 14260 KB -BaekjoonHub

---
+++ 백준/Silver/13241. 최소공배수/최소공배수.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        long a = Long.parseLong(st.nextToken());
+        long b = Long.parseLong(st.nextToken());
+
+        bw.write(lcm(a, b) + "");
+
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+
+    private static long lcm(long a, long b) {
+        return (a * b) / gcd(a, b);
+    }
+
+    private static long gcd(long a, long b) {
+        if (a % b == 0) return b;
+
+        return gcd(b, a % b);
+    }
+}
