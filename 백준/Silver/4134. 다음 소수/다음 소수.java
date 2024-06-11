c18b4cdb9296e5619485f0e73ec41b1d876a8d43
Greta Jiyun Lee <51826219+howecofe@users.noreply.github.com>
2024-06-11 23:19:51 +0900

    [Silver IV] Title: 다음 소수, Time: 444 ms, Memory: 14800 KB -BaekjoonHub

--- 백준/Silver/4134. 다음 소수/다음 소수.java
+++ 백준/Silver/4134. 다음 소수/다음 소수.java
@@ -7,8 +7,17 @@ class Main {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int T = Integer.parseInt(br.readLine());
+        long answer = 0;
+
         for (int i = 0; i < T; i++) {
-            long answer = isPrime(Long.parseLong(br.readLine()));
+            long num = Long.parseLong(br.readLine());
+
+            if (num <= 2) { answer = 2; }
+            else {
+                while (!isPrime(num)) { num++; }
+                answer = num;
+            }
+
             bw.write(answer + "\n");
         }
 
@@ -17,22 +26,13 @@ class Main {
         bw.close();
     }
 
-    private static long isPrime(long n) {
-        if (n <= 2) return 2;
-        boolean isPrime = false;
-
-        while (!isPrime) {
-            isPrime = true;
-            for (int i = 2; i <= Math.sqrt(n); i++) {
-                if (n % i == 0) {
-                    isPrime = false;
-                    break;
-                }
+    private static boolean isPrime(long n) {
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                return false;
             }
-            n++;
         }
-
-        return n - 1;
+        return true;
     }
 
 }
