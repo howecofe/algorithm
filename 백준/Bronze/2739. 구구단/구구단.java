3b0c6e8006f10183b4d69965e71cfce7020aa737
Greta Jiyun Lee <51826219+howecofe@users.noreply.github.com>
2024-07-12 10:35:24 +0900

    [Bronze V] Title: 구구단, Time: 108 ms, Memory: 14480 KB -BaekjoonHub

--- 백준/Bronze/2739. 구구단/구구단.java
+++ 백준/Bronze/2739. 구구단/구구단.java
@@ -1,12 +1,17 @@
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int dan = sc.nextInt();
 
-        for (int i = 1; i <= 9; i++) {
-            System.out.printf("%d * %d = %d\n", dan, i, dan * i);
-        }
-    }
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		for (int i = 1; i <= 9; i++) {
+			System.out.printf("%d * %d = %d\n", N, i, N * i);
+		}
+		
+		br.close();
+	}
+
 }
