9dcff9edd47ac9cba67512e3c257e71b1222a1ff
Greta Jiyun Lee <51826219+howecofe@users.noreply.github.com>
2024-07-12 10:44:42 +0900

    [Bronze I] Title: 2007년, Time: 104 ms, Memory: 14252 KB -BaekjoonHub

---
+++ 백준/Bronze/1924. 2007년/2007년.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	static String[] dayOfWeek = {"SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"};
+	static int x, y;
+	static int[] days = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		x = Integer.parseInt(st.nextToken());
+		y = Integer.parseInt(st.nextToken());
+		
+		int sum = 0;
+		for (int i = 0; i < x - 1; i++) {
+			sum += days[i];
+		}
+		sum += y;
+		
+		bw.write(dayOfWeek[sum % 7]);
+		
+		br.close();
+		bw.flush();
+		bw.close();
+	}
+
+}
