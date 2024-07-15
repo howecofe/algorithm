70076eb304412383e801ed5f3885578b3327b774
Greta Jiyun Lee <51826219+howecofe@users.noreply.github.com>
2024-07-15 10:53:41 +0900

    [Bronze III] Title: 과제 안 내신 분..?, Time: 124 ms, Memory: 14060 KB -BaekjoonHub

--- 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.java
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.java
@@ -1,24 +1,22 @@
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int totalCnt = 30;
-        int submittedCnt = 28;
-        int[] arr = new int[totalCnt + 1];
 
-        for (int i = 0; i < submittedCnt; i++) {
-            int studentNum = sc.nextInt();
-            arr[studentNum]++;
-        }
+	static int[] arr = new int[31];
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		for (int i = 0; i < 28; i++) {
+			arr[Integer.parseInt(br.readLine())]++;
+		}
+		
+		for (int i = 1; i <= 30; i++) {
+			if (arr[i] == 0) System.out.println(i);
+		}
+		
+		br.close();
+	}
 
-        List<Integer> list = new ArrayList<>();
-        for (int i = 1; i < arr.length; i++) {
-            if (arr[i] == 0) { list.add(i); }
-        }
-
-        Collections.sort(list);
-
-        for (int n : list) { System.out.println(n); }
-    }
 }
