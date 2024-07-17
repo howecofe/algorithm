5bbae0e5dd7656fdc504a8b77cf23e7112b94961
Greta Jiyun Lee <51826219+howecofe@users.noreply.github.com>
2024-07-17 13:02:00 +0900

    [Bronze III] Title: 최댓값, Time: 124 ms, Memory: 15836 KB -BaekjoonHub

--- 백준/Bronze/2566. 최댓값/최댓값.java
+++ 백준/Bronze/2566. 최댓값/최댓값.java
@@ -1,20 +1,39 @@
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int max = -1;
-        int order = -1;
 
-        for (int i = 0; i < 81; i++) {
-            int n = sc.nextInt();
-            if (n > max) {
-                max = n;
-                order = i;
-            }
-        }
+	static int max;
+	static int[] pos = new int[2];
+	static int[][] arr = new int[9][9];
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        System.out.println(max);
-        System.out.printf("%d %d", order / 9 + 1, order % 9 + 1);
-    }
+		// 풀이
+		max = -1;
+		for (int i = 1; i <= 9; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			for (int j = 1; j <= 9; j++) {
+				int n = Integer.parseInt(st.nextToken());
+				
+				if (max < n) {
+					max = n;
+					pos[0] = i;
+					pos[1] = j;
+				}
+			}
+		
+		}
+		
+		// 출력
+		System.out.println(max);
+		System.out.println(pos[0] + " " + pos[1]);
+				
+		br.close();
+	}
+
 }
