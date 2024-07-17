36a0ba224c3cbcb8225e587bcf6f1d42905fb1ee
Greta Jiyun Lee <51826219+howecofe@users.noreply.github.com>
2024-07-17 12:53:47 +0900

    [Silver V] Title: 색종이, Time: 104 ms, Memory: 14188 KB -BaekjoonHub

--- 백준/Silver/2563. 색종이/색종이.java
+++ 백준/Silver/2563. 색종이/색종이.java
@@ -1,29 +1,40 @@
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int[][] arr = new int[100][100];
-        int answer = 0;
 
-        for (int i = 0; i < n; i++) {
-            int x = sc.nextInt();
-            int y = sc.nextInt();
+	static int N, cnt;
+	static int[][] arr = new int[100][100];
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		N = Integer.parseInt(br.readLine());
 
-            for (int j = x; j < x + 10; j++) {
-                for (int k = y; k < y + 10; k++) {
-                    arr[j][k] = 1;
-                }
-            }
-        }
+		for (int i = 0; i < N; i++) {
+			String[] str = br.readLine().split(" ");
+			int x = Integer.parseInt(str[0]);
+			int y = Integer.parseInt(str[1]);
+			
+			for (int j = y; j < y + 10; j++) {
+				for (int k = x; k < x + 10; k++) {
+					arr[j][k] = 1;
+				}
+			}
+		}
+		
+		for (int i = 0; i < 100; i++) {
+			for (int j = 0; j < 100; j++) {
+				if (arr[i][j] == 1) cnt++;
+			}
+		}
+		
+		System.out.println(cnt);
+				
+		br.close();
+	}
 
-        for (int[] nums : arr) {
-            for (int num : nums) {
-                if (num == 1) answer++;
-            }
-        }
-
-        System.out.println(answer);
-    }
 }
