9614ed089a8a48a46b3a4664ccb138e5031997f3
Greta Jiyun Lee <51826219+howecofe@users.noreply.github.com>
2024-07-17 12:32:09 +0900

    [Bronze III] Title: 행렬 덧셈, Time: 384 ms, Memory: 19256 KB -BaekjoonHub

--- 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.java
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.java
@@ -1,31 +1,41 @@
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int m = sc.nextInt();
-        int[][] a = new int[n][m];
-        int[][] b = new int[n][m];
 
-        for (int i = 0; i < a.length; i++) {
-            for (int j = 0; j < a[i].length; j++) {
-                a[i][j] = sc.nextInt();
-            }
-        }
+	static int N, M;
+	static int[][] arr;
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		// 입력
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		arr = new int[N][M];
 
-        for (int i = 0; i < b.length; i++) {
-            for (int j = 0; j < b[i].length; j++) {
-                b[i][j] = sc.nextInt();
-            }
-        }
+		// 풀이
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < M; j++) {
+				arr[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < M; j++) {
+				arr[i][j] += Integer.parseInt(st.nextToken());
+				System.out.print(arr[i][j] + " ");
+			}
+			System.out.println();
+		}
+		
+		
+		br.close();
+	}
 
-        for (int i = 0; i < a.length; i++) {
-            String answer = "";
-            for (int j = 0; j < a[i].length; j++) {
-                answer += (a[i][j] + b[i][j]) + " ";
-            }
-            System.out.println(answer.trim());
-        }
-    }
 }
