acfdbdcda714452aea7f6c4fc681a418916be2db
Greta Jiyun Lee <51826219+howecofe@users.noreply.github.com>
2024-07-19 12:57:39 +0900

    [Silver V] Title: 소트인사이드, Time: 124 ms, Memory: 14412 KB -BaekjoonHub

--- 백준/Silver/1427. 소트인사이드/소트인사이드.java
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.java
@@ -1,16 +1,33 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.StringTokenizer;
 
 class Main {
+    static char[] arr;
+    static int[] size = new int[6];
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        String[] str = br.readLine().split("");
-        Arrays.sort(str, Comparator.reverseOrder());
 
-        for (String s : str) {
-            bw.write(s);
+        // 입력
+        arr = br.readLine().toCharArray();
+
+        // 풀이
+        Arrays.sort(arr);
+
+        StringBuilder sb = new StringBuilder();
+
+        for (char c : arr) {
+            sb.insert(0, c);
         }
+
+        // 출력
+        bw.write(sb.toString());
 
         br.close();
         bw.flush();
