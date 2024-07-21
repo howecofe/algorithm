b98038bf7565b2d0804490226595512df13c743a
Greta Jiyun Lee <51826219+howecofe@users.noreply.github.com>
2024-07-21 23:09:07 +0900

    [level 1] Title: 폰켓몬, Time: 6.53 ms, Memory: 80.4 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/README.md
+++ 프로그래머스/1/1845. 폰켓몬/README.md
@@ -1,10 +1,10 @@
 # [level 1] 폰켓몬 - 1845 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1845?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1845) 
 
 ### 성능 요약
 
-메모리: 79.9 MB, 시간: 5.26 ms
+메모리: 80.4 MB, 시간: 6.53 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 28일 22:19:21
+2024년 07월 21일 23:09:04
 
 ### 문제 설명
 
--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -2,11 +2,43 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] nums) {
-        Set<Integer> set = new HashSet();
-        for (int n : nums) { set.add(n); }
-        
-        // if (set.size() <= nums.length / 2) { return set.size(); }
-        // else { return nums.length / 2; }
+        Set<Integer> set = new HashSet<>();
+        for (int n : nums) set.add(n);
+
         return Math.min(set.size(), nums.length / 2);
     }
+
 }
+
+// 조합 풀이
+//class Solution {
+//    static int[] tgt;
+//    static int[] src;
+//    static int N, max;
+//    public int solution(int[] nums) {
+//        // N개 중 N/2개 고르는 조합
+//        N = nums.length;
+//        src = Arrays.copyOf(nums, N);
+//        tgt = new int[N / 2];
+//        comb(0, 0);
+//
+//        return max;
+//    }
+//
+//    static void comb(int srcIdx, int tgtIdx) {
+//        if (tgtIdx == tgt.length) {
+//            // 조합 1개 완성
+//            Set<Integer> set = new HashSet<>();
+//            for (int n : tgt) set.add(n);
+//            max = Math.max(max, set.size());
+//            return;
+//        }
+//
+//        if (srcIdx == N) return;
+//
+//        tgt[tgtIdx] = src[srcIdx];
+//        comb(srcIdx + 1, tgtIdx + 1);
+//        comb(srcIdx + 1, tgtIdx);
+//    }
+//
+//}
