ba92090a133fb919361f87167b8e7fae3af01671
Greta Jiyun Lee <51826219+howecofe@users.noreply.github.com>
2024-07-22 23:49:37 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 88.72 ms, Memory: 95.2 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,37 +1,23 @@
 import java.util.*;
 
 class Solution {
+    static Map<String, Integer> map = new HashMap<>();
+    static String answer;
     public String solution(String[] participant, String[] completion) {
-        
-        // 1. 정렬 이용하기
-//         // Note. i를 전역변수로 선언 후, for문 내에서도 사용한다.
-//         // int i = 0; 
-//         // if (completion.length == 0) { return participant[0]; }
-        
-//         Arrays.sort(participant);
-//         Arrays.sort(completion);
-        
-//         // if (!completion[completion.length - 1].equals(participant[participant.length - 1])) {
-//         //     return participant[participant.length - 1];
-//         // }
-        
-//         for (int i = 0; i < completion.length; i++) {
-//             if (!participant[i].equals(completion[i])) { return participant[i]; }
-//         }
-        
-//         return participant[participant.length - 1];
-        
-        // 2. HashMap 이용하기
-        String answer = "";
-        Map<String, Integer> map = new HashMap<>();
-        
-        for (String player : participant) { map.put(player, map.getOrDefault(player, 0) + 1); }
-        for (String player : completion) { map.put(player, map.get(player) - 1); }
-        
-        for (String key : map.keySet()) {
-            if (map.get(key) != 0) { answer = key; }
+        // 마라톤에 참여한 선수들의 이름이 담긴 배열 participant
+        // 완주한 선수들의 이름이 담긴 배열 completion
+        // 단 한 명의 선수를 제외하고는 모든 선수가 마라톤을 완주
+        // 완주하지 못한 선수의 이름을 return
+
+        // 풀이
+        for (String p : participant) map.put(p, map.getOrDefault(p, 0) + 1);
+        for (String c : completion) map.put(c, map.get(c) - 1);
+
+        // 출력
+        for (Map.Entry<String, Integer> e : map.entrySet()) {
+            if (e.getValue() == 1) answer = e.getKey();
         }
-        
+
         return answer;
     }
 }
