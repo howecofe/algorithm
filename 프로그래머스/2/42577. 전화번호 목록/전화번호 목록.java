59da8c684380f26ddadad1175699f1b88785a510
Greta Jiyun Lee <51826219+howecofe@users.noreply.github.com>
2024-07-23 00:32:28 +0900

    [level 2] Title: 전화번호 목록, Time: 340.04 ms, Memory: 98.7 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -1,19 +1,16 @@
 import java.util.*;
 
 class Solution {
-    static Map<String, Integer> map = new HashMap<>();
     public boolean solution(String[] phone_book) {
         // 전화번호를 담은 배열 phone_book
         // 어떤 번호가 다른 번호의 접두어인 경우가 있으면 false를 그렇지 않으면 true를 return
 
         // 풀이
-        for (String ph : phone_book) map.put(ph, 0);
-        
-        for (String ph : phone_book) {
-            for (int i = 1; i < ph.length(); i++) { // 자기 자신은 제외해야하므로 등호 제외
-                if (map.containsKey(ph.substring(0, i))) return false;
-            }
+        Arrays.sort(phone_book);
+        for (int i = 0; i < phone_book.length - 1; i++) {
+            if (phone_book[i + 1].startsWith(phone_book[i])) return false;
         }
+
         return true;
     }
 }
