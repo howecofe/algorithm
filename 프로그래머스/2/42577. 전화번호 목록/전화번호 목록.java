5994ee442ac0b5846a3865ea67ff443324c6cc60
Greta Jiyun Lee <51826219+howecofe@users.noreply.github.com>
2024-07-23 00:12:45 +0900

    [level 2] Title: 전화번호 목록, Time: 266.49 ms, Memory: 250 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -1,35 +1,19 @@
 import java.util.*;
 
 class Solution {
+    static Map<String, Integer> map = new HashMap<>();
     public boolean solution(String[] phone_book) {
-        // 1. 문자열 정렬 풀이
-//         Arrays.sort(phone_book);
+        // 전화번호를 담은 배열 phone_book
+        // 어떤 번호가 다른 번호의 접두어인 경우가 있으면 false를 그렇지 않으면 true를 return
+
+        // 풀이
+        for (String ph : phone_book) map.put(ph, 0);
         
-//         for (int i = 0; i < phone_book.length - 1; i++) {
-//             // if (phone_book[i].equals(phone_book[i + 1].substring(0, phone_book[i].length()))) {
-//             //     return false;
-//             // }
-//             if (phone_book[i + 1].startsWith(phone_book[i])) {
-//                 return false;
-//             }
-//         }
-        
-//         return true;
-        
-        // 2. 해시맵 풀이
-        Map<String, Integer> map = new HashMap<>();
-        for (int i = 0; i < phone_book.length; i++) {
-            map.put(phone_book[i], i);
-        }
-        
-        for (String hp : phone_book) {
-            for (int i = 1; i < hp.length(); i++) { // 같은 번호가 중복해서 들어있지 않으므로 i 범위에서 hp.length()는 제외 (자기 자신 제외)
-                if (map.containsKey(hp.substring(0, i))) {
-                    return false;
-                }
+        for (String ph : phone_book) {
+            for (int i = 1; i < ph.length(); i++) { // 자기 자신은 제외해야하므로 등호 제외
+                if (map.containsKey(ph.substring(0, i))) return false;
             }
         }
-        
         return true;
     }
 }
