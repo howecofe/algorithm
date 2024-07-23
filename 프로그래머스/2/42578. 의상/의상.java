965d4b4b3b5f34fa36605b59589f081d2f0981ea
Greta Jiyun Lee <51826219+howecofe@users.noreply.github.com>
2024-07-23 21:13:15 +0900

    [level 2] Title: 의상, Time: 0.11 ms, Memory: 81.1 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -1,23 +1,23 @@
 import java.util.*;
 
 class Solution {
+    static int cnt;
+    static Map<String, Integer> map = new HashMap<>();
     public int solution(String[][] clothes) {
-        // 방정식 찾기
-        // 종류 1가지: a개 = (a + 1) - 1
-        // 종류 2가지: a + b + ab = (a + 1)(b + 1) -1
-        // 종류 3가지: a + b + c + ab + ac + bc + abc = (a + 1)(b + 1)(c + 1) - 1
-           
-        Map<String, Integer> map = new HashMap<>();
+        // 의상들이 담긴 2차원 배열 clothes
+        // 서로 다른 옷의 조합의 수를 return
+
+        // 1개 : a = (a + 1) - 1
+        // 2개 : a + b + ab = (a + 1)(b + 1) - 1
+        // 3개 : a + b + c + ab + ac + bc + abc= (a + 1)(b + 1)(c + 1) - 1 
         
-        for (String[] c : clothes) {
-            map.put(c[1], map.getOrDefault(c[1], 0) + 1);
+        for (String[] c : clothes) map.put(c[1], map.getOrDefault(c[1], 0) + 1);
+        cnt = 1;
+        
+        for (String key : map.keySet()) {
+            cnt *= map.get(key) + 1;
         }
         
-        int answer = 1;
-        for (Integer val : map.values()) {
-            answer *= val + 1;
-        }
-        
-        return --answer;
+        return cnt - 1;
     }
 }
