9888c6581cc8b47cdaf3384b103eb11136261fd6
Greta Jiyun Lee <51826219+howecofe@users.noreply.github.com>
2024-07-24 14:20:04 +0900

    [level 2] Title: 올바른 괄호, Time: 15.84 ms, Memory: 53.2 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -1,23 +1,25 @@
+import java.lang.reflect.Array;
 import java.util.*;
 
 class Solution {
+    static ArrayDeque<Character> stk = new ArrayDeque<>();
+    static char[] arr;
     boolean solution(String s) {
-        if (s.charAt(0) == ')') { return false; }
-        
-        Stack<Character> stack = new Stack<>();
-        
-        for (char c : s.toCharArray()) {
-            if (c == ')') {
-                if (!stack.isEmpty() && stack.peek() == '(') {
-                    stack.pop();
-                }
-            } else {
-                stack.push(c);
+        // '(' 또는 ')' 로만 이루어진 문자열 s
+        // s가 올바른 괄호이면 true를 return 하고, 올바르지 않은 괄호이면 false를 return
+
+        arr = s.toCharArray();
+        for (char c : arr) {
+            if (c == '(') stk.push(c);
+            else { // c == ')'
+                if (stk.isEmpty() || stk.peek() == ')') return false;
+                
+                stk.pop(); // '('이면 꺼내기
             }
         }
-        
-        if (stack.size() == 0) { return true; }
-    
-        return false;
+
+        if (!stk.isEmpty()) return false;
+
+        return true;
     }
 }
