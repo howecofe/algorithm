3f1236120ef75d38091c4809fdf6e2d87b67db37
Greta Jiyun Lee <51826219+howecofe@users.noreply.github.com>
2024-07-24 13:44:58 +0900

    [level 2] Title: 기능개발, Time: 5.29 ms, Memory: 83.9 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -1,43 +1,46 @@
 import java.util.*;
 
 class Solution {
+    static LinkedList<Integer> pq = new LinkedList<>();
+    static LinkedList<Integer> sq = new LinkedList<>();
+    static List<Integer> cntList = new ArrayList<>();
+    static int[] ans;
     public int[] solution(int[] progresses, int[] speeds) {
-        List<Integer> list = new ArrayList<>();
-        LinkedList<Integer> q = new LinkedList<>();
-        
-        for (int i = 0; i < progresses.length; i++) {
-            q.offer(progresses[i]);
-        }
-        
-        int idx = 0;
-        int days = 0;
-        
-        while (!q.isEmpty()) {
-            int progress = progresses[idx];
-            int speed = speeds[idx];
-            
-            days = (100 - progress) % speed == 0 ?
-                (100 - progress) / speed
-                : (100 - progress) / speed + 1;
-            // days = (int) Math.ceil((double)(100 - progress) / speed);
-            
-            int completedTasks = 0;
-            while (!q.isEmpty() && q.peek() + speeds[idx] * days >= 100) {
-                completedTasks++;
-                q.poll();
-                idx++;
+        // 먼저 배포되어야 하는 순서대로 작업의 진도가 적힌 정수 배열 progresses
+        // 각 작업의 개발 속도가 적힌 정수 배열 speeds
+        // 각 배포마다 몇 개의 기능이 배포되는지를 return
+        // 뒤에 있는 기능이 앞에 있는 기능보다 먼저 개발될 수 있고, 이때 뒤에 있는 기능은 앞에 있는 기능이 배포될 때 함께 배포
+
+        // 풀이
+        // 맨 앞 기능이 100 이상이 될 때까지 순회하면서 작업
+        // 맨 앞 기능이 100 이상이 되면, 100을 넘는 작업들 꺼내기
+
+        for (int p : progresses) pq.offer(p);
+        for (int s : speeds) sq.offer(s);
+
+        while (!pq.isEmpty()) {
+            for (int i = 0; i < pq.size(); i++) {
+                int p = pq.poll();
+                int s = sq.poll();
+                pq.offer(p + s);
+                sq.offer(s);
             }
-            
-            if (completedTasks > 0) {
-                list.add(completedTasks);
+
+            int cnt = 0;
+            // 완료된 작업(100)까지만 빼내기
+            while (pq.peek() != null && pq.peek() >= 100) {
+                cnt++;
+                pq.poll();
+                sq.poll();
             }
+
+            if (cnt > 0) cntList.add(cnt);
         }
-        
-        int[] answer = new int[list.size()];
-        for (int i = 0; i < answer.length; i++) {
-            answer[i] = list.get(i);
-        }
-        
-        return answer;
+
+        // list -> array
+        ans = new int[cntList.size()];
+        for (int i = 0; i < ans.length; i++) ans[i] = cntList.get(i);
+
+        return ans;
     }
 }
