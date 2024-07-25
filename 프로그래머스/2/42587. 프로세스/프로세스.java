4a01ffe70e2fe04d5f156e71e60491ea0c2aa176
Greta Jiyun Lee <51826219+howecofe@users.noreply.github.com>
2024-07-25 15:02:29 +0900

    [level 2] Title: 프로세스, Time: 1.64 ms, Memory: 73.8 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.java
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -1,37 +1,36 @@
 import java.util.*;
 
 class Solution {
+    static int cnt;
+    static LinkedList<Integer> pq = new LinkedList<>(); // 우선순위 담는 큐
+    static LinkedList<Integer> iq = new LinkedList<>(); // idx 담는 큐
     public int solution(int[] priorities, int location) {
-        int answer = 0;
-        LinkedList<Integer> proQueue = new LinkedList<>();
-        LinkedList<Integer> orderQueue = new LinkedList<>();
-        List<Integer> order = new ArrayList<>();
-        
+        // 프로세스의 중요도가 순서대로 담긴 배열 priorities : 숫자가 클수록 우선순위가 높다.
+        // 몇 번째로 실행되는지 알고싶은 프로세스의 위치를 알려주는 location
+        // 해당 프로세스가 몇 번째로 실행되는지 return
+
         for (int i = 0; i < priorities.length; i++) {
-            proQueue.offer(priorities[i]);
-            orderQueue.offer(i);
+            pq.offer(priorities[i]);
+            iq.offer(i);
         }
-        
-        while (!proQueue.isEmpty()) {
-            int proVal = proQueue.poll();
-            int ordVal = orderQueue.poll();
-            boolean exists = false;
-            
-            for (int i = 0; i < proQueue.size(); i++) {
-                if (proVal < proQueue.get(i)) {
-                    exists = true;
+
+        Arrays.sort(priorities);
+        for (int i = priorities.length - 1; i >= 0; i--) {
+            while (!pq.isEmpty()) {
+                int p = pq.poll();
+                int idx = iq.poll();
+
+                if (p == priorities[i]) {
+                    cnt++;
+                    if (idx == location) return cnt;
                     break;
+                } else {
+                    pq.offer(p);
+                    iq.offer(idx);
                 }
             }
-            
-            if (exists) {
-                proQueue.offer(proVal);
-                orderQueue.offer(ordVal);
-            } else {
-                order.add(ordVal);
-            }
         }
-        
-        return order.indexOf(location) + 1;
+
+        return cnt;
     }
 }
