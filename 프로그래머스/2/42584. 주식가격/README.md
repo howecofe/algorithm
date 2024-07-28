cabc98241cead55996ad598733f2baa5703a64b5
Greta Jiyun Lee <51826219+howecofe@users.noreply.github.com>
2024-07-29 06:32:17 +0900

    [level 2] Title: 주식가격, Time: 25.16 ms, Memory: 74.6 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/README.md
+++ 프로그래머스/2/42584. 주식가격/README.md
@@ -1,10 +1,10 @@
 # [level 2] 주식가격 - 42584 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42584) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42584#qna) 
 
 ### 성능 요약
 
-메모리: 68.6 MB, 시간: 17.11 ms
+메모리: 74.6 MB, 시간: 25.16 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 07월 28일 23:17:26
+2024년 07월 29일 06:32:15
 
 ### 문제 설명
 
--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -1,14 +1,24 @@
-class Solution {
-    public int[] solution(int[] prices) {
-        int[] secArr = new int[prices.length];
+import java.util.*;
 
+class Solution {
+    public static int[] solution(int[] prices) {
+        int[] secArr = new int[prices.length];
+        ArrayDeque<Integer> stk = new ArrayDeque<>(); // (주식가격, 인덱스) 저장
+
+        // 현재 주식가격과 스택의 주식가격을 비교해서, 하락장이 나타나면 (현재 인덱스 - 스택의 해당 인덱스)로 secArr 갱신 후 스택에서 제거
         for (int i = 0; i < prices.length; i++) {
-            int sec = 0;
-            for (int j = i + 1; j < prices.length; j++) {
-                sec++;
-                if (prices[i] > prices[j]) break;
+            while (!stk.isEmpty() && prices[i] < prices[stk.peek()]) {
+                int idx = stk.pop();
+                secArr[idx] = i - idx;
             }
-            secArr[i] = sec;
+
+            stk.push(i);
+        }
+
+        // 끝까지 하락하지 않은 주식가격들 처리
+        while(!stk.isEmpty()) {
+            int idx = stk.pop();
+            secArr[idx] = prices.length - idx - 1;
         }
 
         return secArr;
