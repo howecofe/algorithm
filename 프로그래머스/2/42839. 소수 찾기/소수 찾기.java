f266d7a11f67efb1cf0dc1a29669e1425f3d012a
Greta Jiyun Lee <51826219+howecofe@users.noreply.github.com>
2024-07-31 22:13:21 +0900

    [level 2] Title: 소수 찾기, Time: 29.20 ms, Memory: 91.9 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -2,83 +2,73 @@ import java.util.*;
 
 class Solution {
     // 1. DFS 사용 풀이
-    static boolean[] visited;
-    static char[] nums;
-    static Set<Integer> set = new HashSet<>();
-    static int cnt;
-        
-    public static int solution(String numbers) {
-        nums = numbers.toCharArray();
-        visited = new boolean[nums.length];
-
-        // 가능한 조합 구하기
-        dfs(0, "");
-
-        // 소수 판별
-        for (int n : set) {
-            if (isPrime(n)) cnt++;
-        }
-
-        return cnt;
-    }
-
-    static void dfs(int depth, String s) {
-        // 기저조건
-        if (depth == nums.length) return;
-
-        for (int i = 0; i < nums.length; i++) {
-            // 방문 여부 체크
-            if (visited[i]) continue;
-
-            // set에 추가하고 방문 처리
-            set.add(Integer.parseInt(s + nums[i]));
-            visited[i] = true;
-
-            // 다음 방문 이어 나가기
-            dfs(depth + 1, s + nums[i]);
-
-            // 재귀호출로 리프노드까지 다녀온 후, 방문 처리 원복
-            visited[i] = false;
-        }
-    }
-
-    static boolean isPrime(int n) {
-        if (n < 2) return false;
-
-        for (int i = 2; i <= Math.sqrt(n); i++) {
-            if (n % i == 0) return false;
-        }
-
-        return true;
-    }
-
-    // 2. 카운트 배열 사용 풀이
+//     static Set<Integer> set = new HashSet<>();
+//     static boolean[] visited = new boolean[7];
+    
 //     public int solution(String numbers) {
 //         int answer = 0;
-//         int[] nums = new int[10];
         
-//         for (String s : numbers.split("")) {
-//             nums[Integer.parseInt(s)]++; // 카운트 배열
-//         }
+//         dfs(numbers, "", 1);
         
-//         for (int i = 2; i <= 9999999; i++) {
-//             int[] newArr = nums.clone(); // 카운트 배열 복사가 핵심.
-//             int target = i;
-//             boolean isContain = true; // 종이 조각을 붙여 만들 수 있는 수인지 확인하는 변수
-            
-//             while (target > 0) {
-//                 if (newArr[target % 10] == 0) {
-//                     isContain = false;
-//                     break;
-//                 }
-                
-//                 newArr[target % 10]--;
-//                 target /= 10;
-//             }
-            
-//             if (isContain && isPrime(i)) { answer++; }
+//         for (int n : set) {
+//             if (isPrime(n)) { answer++; }
 //         }
         
 //         return answer;
 //     }
+    
+//     private static void dfs(String numbers, String tmp, int depth) {
+//         if (depth > numbers.length()) { return; }
+
+//         for (int i = 0; i < numbers.length(); i++) {
+//             if (!visited[i]) {
+//                 visited[i] = true;
+//                 set.add(Integer.parseInt(tmp + numbers.charAt(i)));
+//                 dfs(numbers, tmp + numbers.charAt(i), depth + 1);
+//                 visited[i] = false;
+//             }
+//         }
+//     }
+
+    // 2. 카운트 배열 사용 풀이
+    public int solution(String numbers) {
+        int answer = 0;
+        int[] nums = new int[10];
+        
+        for (String s : numbers.split("")) {
+            nums[Integer.parseInt(s)]++; // 카운트 배열
+        }
+        
+        for (int i = 2; i <= 9999999; i++) {
+            int[] newArr = nums.clone(); // 카운트 배열 복사가 핵심.
+            int target = i;
+            boolean isContain = true; // 종이 조각을 붙여 만들 수 있는 수인지 확인하는 변수
+            
+            while (target > 0) {
+                if (newArr[target % 10] == 0) {
+                    isContain = false;
+                    break;
+                }
+                
+                newArr[target % 10]--;
+                target /= 10;
+            }
+            
+            if (isContain && isPrime(i)) { answer++; }
+        }
+        
+        return answer;
+    }
+    
+    private static boolean isPrime(int num) {
+        if (num <= 1) { return false; }
+        
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
 }
