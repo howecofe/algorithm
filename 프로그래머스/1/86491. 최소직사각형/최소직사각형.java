965aa3ed7f5fd8209febb6061fb49699f23a4208
Greta Jiyun Lee <51826219+howecofe@users.noreply.github.com>
2024-07-31 20:31:19 +0900

    [level 1] Title: 최소직사각형, Time: 2.26 ms, Memory: 93.4 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
@@ -1,13 +1,12 @@
 class Solution {
     public int solution(int[][] sizes) {
-        int w = Math.max(sizes[0][0], sizes[0][1]);
-        int h = Math.min(sizes[0][0], sizes[0][1]);
-        
-        for (int i = 1; i < sizes.length; i++) {
-            if (w < Math.max(sizes[i][0], sizes[i][1])) { w = Math.max(sizes[i][0], sizes[i][1]); }
-            if (h < Math.min(sizes[i][0], sizes[i][1])) { h = Math.min(sizes[i][0], sizes[i][1]); }
+        int mw = 0, mh = 0;
+
+        for (int[] s : sizes) {
+            mw = Math.max(mw, Math.max(s[0], s[1]));
+            mh = Math.max(mh, Math.min(s[0], s[1]));
         }
         
-        return w * h;
+        return mw * mh;
     }
 }
