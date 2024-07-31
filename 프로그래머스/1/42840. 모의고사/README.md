7148a880fd402e389bfb712e46ec5cffb8dd46b2
Greta Jiyun Lee <51826219+howecofe@users.noreply.github.com>
2024-07-31 20:58:05 +0900

    [level 1] Title: 모의고사, Time: 1.34 ms, Memory: 83.9 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/README.md
+++ 프로그래머스/1/42840. 모의고사/README.md
@@ -1,10 +1,10 @@
 # [level 1] 모의고사 - 42840 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42840?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42840) 
 
 ### 성능 요약
 
-메모리: 79.3 MB, 시간: 0.62 ms
+메모리: 83.9 MB, 시간: 1.34 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 28일 23:12:01
+2024년 07월 31일 20:58:03
 
 ### 문제 설명
 
--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -2,61 +2,33 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] answers) {
-//         int[] scores = new int[3];
-//         for (int i = 0; i < answers.length; i++) {
-//             if (answers[i] == i % 5 + 1) { scores[0]++; }
-            
-//             int[] arr1 = new int[]{1, 3, 4, 5};
-//             if (i % 2 == 0 && answers[i] == 2) { scores[1]++; }
-//             else if (i % 2 == 1 && answers[i] == arr1[(i / 2) % 4]) { scores[1]++; }
-            
-//             int[] arr2 = new int[]{3, 1, 2, 4, 5};
-//             if (answers[i] == arr2[(i / 2) % 5]) { scores[2]++; }
-//         }
-        
-//         // max 점수가 3명인 경우
-//         if (scores[0] == scores[1] && scores[1] == scores[2]) { return new int[]{1, 2, 3}; }
-        
-//         int max = scores[0];
-//         int idx = 0;
-//         for (int i = 0; i < scores.length; i++) {
-//             if (max < scores[i]) {
-//                 max = scores[i];
-//                 idx = i;
-//             }
-//         }
-        
-//         for (int i = 0; i < scores.length; i++) {
-//             // max 점수가 2명인 경우
-//             if (i != idx && max == scores[i]) {
-//                 return new int[]{Math.min(i, idx) + 1, Math.max(i, idx) + 1};
-//             }
-//         }
-        
-//         // max 점수가 1명인 경우
-//         return new int[]{idx + 1};
-        
-        int[] pattern1 = new int[]{1, 2, 3, 4, 5};
-        int[] pattern2 = new int[]{2, 1, 2, 3, 2, 4, 2, 5};
-        int[] pattern3 = new int[]{3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
-        int[] hit = new int[3];
-        
-        for (int i = 0; i < answers.length; i++) {
-            if (answers[i] == pattern1[i % pattern1.length]) { hit[0]++; }
-            if (answers[i] == pattern2[i % pattern2.length]) { hit[1]++; }
-            if (answers[i] == pattern3[i % pattern3.length]) { hit[2]++; }
-        }
-        
-        int max = Math.max(hit[0], Math.max(hit[1], hit[2]));
         List<Integer> list = new ArrayList<>();
-        
-        for (int i = 0; i < hit.length; i++) {
-            if (max == hit[i]) { list.add(i + 1); }
+        int[] arr1 = {1, 2, 3, 4, 5};
+        int[] arr2 = {2, 1, 2, 3, 2, 4, 2, 5};
+        int[] arr3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
+        int[] score = new int[4]; // 0: dummy
+
+        // 정답 개수 구하기
+        for (int i = 0; i < answers.length; i++) {
+            if (answers[i] == arr1[i % 5]) score[1]++;
+            if (answers[i] == arr2[i % 8]) score[2]++;
+            if (answers[i] == arr3[i % 10]) score[3]++;
         }
-        
-        int[] answer = new int[list.size()];
-        for (int i = 0; i < answer.length; i++) { answer[i] = list.get(i); }
-        
-        return answer;
+
+        // 최댓값 구하기
+        int[] copied = Arrays.copyOf(score, 4);
+        Arrays.sort(copied);
+        int max = copied[3];
+
+        // 1등 구하기
+        for (int i = 1; i <= 3; i++) {
+            if (score[i] == max) list.add(i);
+        }
+
+        // list -> array
+        int[] res = new int[list.size()];
+        for (int i = 0; i < res.length; i++) res[i] = list.get(i);
+
+        return res;
     }
 }
