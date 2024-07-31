de215b937a1d881af503b996a9c2d96f1b9bdaba
Greta Jiyun Lee <51826219+howecofe@users.noreply.github.com>
2024-08-01 01:07:53 +0900

    [level 2] Title: 카펫, Time: 0.11 ms, Memory: 71.4 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -1,17 +1,24 @@
 class Solution {
+    static int[] ans = new int[2]; // w, h
+    
     public int[] solution(int brown, int yellow) {
-        int width = 0, height = 0;
-        
-        for (int i = 1; i <= yellow; i++) {
+
+        // yellow의 (w,h) 조합을 구하기 위해 약수 구하기
+        for (int i = 1; i <= Math.sqrt(yellow); i++) {
             if (yellow % i == 0) {
-                height = i + 2;
-                width = yellow / i + 2;
-                if (height * width - yellow == brown) {
+                // w >= h
+                int h = i + 2;
+                int w = yellow / i + 2;
+                int brownCnt = w * h - yellow;
+
+                if (brownCnt == brown) {
+                    ans[0] = w;
+                    ans[1] = h;
                     break;
                 }
             }
         }
-        
-        return new int[]{width, height};
+
+        return ans;
     }
 }
