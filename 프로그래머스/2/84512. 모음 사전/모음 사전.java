7e5e261fb6effae2cbaa7ff5ad25598d3cc59020
Greta Jiyun Lee <51826219+howecofe@users.noreply.github.com>
2024-08-01 13:00:05 +0900

    [level 2] Title: 모음 사전, Time: 55.80 ms, Memory: 77.4 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.java
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -1,29 +1,32 @@
 import java.util.*;
 
 class Solution {
-    static List<String> list = new ArrayList<>();
-    static char[] alpha = {'A', 'E', 'I', 'O', 'U'};
-    static int answer = 0;
+    static String[] alpha = {"A", "E", "I", "O", "U"};
+    static List<String> dict = new ArrayList<>();
+    static String target;
+    static int res;
     
-    public int solution(String word) {
-        dfs(word, "", 0);
-        answer = list.indexOf(word);
-        
-        return answer;
+    public static int solution(String word) {
+        target = word;
+
+        dfs(0, "");
+
+        return res;
     }
-    
-    private static void dfs(String word, String tmp, int depth) {
-        if (depth > alpha.length) { return; }
-        
-        list.add(tmp);
-        
-        if (list.contains(word)) {
-            answer = list.indexOf(word);
-            return;
-        }
-        
+
+    static void dfs(int depth, String s) {
+        if (depth == alpha.length) return;
+
         for (int i = 0; i < alpha.length; i++) {
-            dfs(word, tmp + alpha[i], depth + 1);
+            dict.add(s + alpha[i]);
+
+            // 찾으려는 단어가 있는지 확인
+            if (dict.contains(target)) {
+                res = dict.indexOf(target) + 1;
+                return;
+            }
+
+            dfs(depth + 1, s + alpha[i]);
         }
     }
 }
