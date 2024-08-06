c7397638b6e2f9b9a18b3d3d8aa7a762862b3837
Greta Jiyun Lee <51826219+howecofe@users.noreply.github.com>
2024-08-07 01:44:48 +0900

    [level 3] Title: 단속카메라, Time: 9.68 ms, Memory: 56.1 MB -BaekjoonHub

--- 프로그래머스/3/42884. 단속카메라/단속카메라.java
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.java
@@ -2,36 +2,22 @@ import java.util.*;
 
 class Solution {
     public int solution(int[][] routes) {
-        List<int[]> camera = new ArrayList<>();
-
-        Arrays.sort(routes, new Comparator<int[]>() {
-            @Override
-            public int compare(int[] o1, int[] o2) {
-                return o1[0] - o2[0];
-            }
-        });
-
+        Arrays.sort(routes, (o1, o2) -> Integer.compare(o1[1], o2[1]));
+        
+        int cam = 0, lastCam = -30001;
         int end = 0, start = 0;
 
         for (int i = 0; i < routes.length; i++) {
             start = routes[i][0];
             end = routes[i][1];
-            boolean replaced = false;
 
-            for (int j = 0; j < camera.size(); j++) {
-                int s = camera.get(j)[0];
-                int e = camera.get(j)[1];
-                if (start <= e) {
-                    int[] arr = {s, e, start, end};
-                    Arrays.sort(arr);
-                    camera.set(j, new int[]{arr[1], arr[2]}); // 교집합으로 갱신
-                    replaced = true;
-                }
+            if (lastCam < start) {
+                cam++;
+                lastCam = end;
             }
 
-            if (!replaced) camera.add(new int[]{start, end});
         }
 
-        return camera.size();
+        return cam;
     }
 }
