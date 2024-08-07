cd7b129de7a6895bf1a4445a29985fc1ad5e8be9
Greta Jiyun Lee <51826219+howecofe@users.noreply.github.com>
2024-08-07 20:54:20 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 7.98 ms, Memory: 52.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -0,0 +1,53 @@
+import java.util.*;
+
+class Solution {
+    static Queue<Node> q = new LinkedList<>();
+    static int ans, n, m;
+    static int[] dy = {1, 0, -1, 0};
+    static int[] dx = {0, 1, 0, -1};
+    
+    public int solution(int[][] maps) {
+        n = maps.length - 1;
+        m = maps[0].length - 1;
+        ans = -1;
+
+        bfs(maps);
+
+        return ans;
+    }
+
+    static class Node {
+        int y, x, c;
+        Node(int y, int x, int c) {
+            this.y = y;
+            this.x = x;
+            this.c = c;
+        }
+    }
+
+    static void bfs(int[][] maps) {
+        q.offer(new Node(0, 0, 1));
+        maps[0][0] = 0;
+
+        while (!q.isEmpty()) {
+            Node node = q.poll();
+
+            for (int d = 0; d < 4; d++) {
+                int newY = node.y + dy[d];
+                int newX = node.x + dx[d];
+                int cost = node.c + 1;
+
+                if (newY < 0 || newY > n || newX < 0 || newX > m || maps[newY][newX] == 0) continue;
+
+                if (newY == m && newX == n) {
+                    ans = cost;
+                    return;
+                }
+
+                maps[newY][newX] = 0;
+                q.offer(new Node(newY, newX, cost));
+            }
+        }
+    }
+
+}
