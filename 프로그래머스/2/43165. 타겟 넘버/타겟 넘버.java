ebf8328aff132f21e64161d6c6759b428ea4e596
Greta Jiyun Lee <51826219+howecofe@users.noreply.github.com>
2024-08-07 10:38:54 +0900

    [level 2] Title: 타겟 넘버, Time: 7.75 ms, Memory: 73.8 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,24 +1,23 @@
 class Solution {
-    static int[] nums;
-    static int targetNum;
-    static int answer = 0;
+    static int ans, tgt;
+    static int[] arr;
     
     public int solution(int[] numbers, int target) {
-        nums = numbers;
-        targetNum = target;
-        
+        arr = numbers.clone();
+        tgt = target;
+
         dfs(0, 0);
-        
-        return answer;
+
+        return ans;
     }
-    
-    private static void dfs(int sum, int depth) {
-        if (depth == nums.length) {
-            if (sum == targetNum) { answer++; }
+
+    static void dfs(int sum, int depth) {
+        if (depth == arr.length) {
+            if (sum == tgt) ans++;
             return;
         }
 
-        dfs(sum + nums[depth], depth + 1);
-        dfs(sum - nums[depth], depth + 1);
+        dfs(sum + arr[depth], depth + 1);
+        dfs(sum - arr[depth], depth + 1);
     }
 }
