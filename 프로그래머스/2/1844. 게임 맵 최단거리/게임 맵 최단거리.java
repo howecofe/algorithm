5856b7c29b05be214f6a4121b860ad1144be3087
Greta Jiyun Lee <51826219+howecofe@users.noreply.github.com>
2024-08-07 21:08:42 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 6.75 ms, Memory: 54.6 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,6 +1,8 @@
 import java.util.*;
 
 class Solution {
+    
+    // 1. BFS (통과)
     static Queue<Node> q = new LinkedList<>();
     static int ans, n, m;
     static int[] dy = {1, 0, -1, 0};
@@ -39,7 +41,7 @@ class Solution {
 
                 if (newY < 0 || newY > n || newX < 0 || newX > m || maps[newY][newX] == 0) continue;
 
-                if (newY == m && newX == n) {
+                if (newY == n && newX == m) {
                     ans = cost;
                     return;
                 }
@@ -49,5 +51,40 @@ class Solution {
             }
         }
     }
+    
+    // 2. DFS (시간초과)
+//     static int ans, n, m;
+//     static int[] dy = {1, 0, -1, 0};
+//     static int[] dx = {0, 1, 0, -1};
+    
+//     public int solution(int[][] maps) {
+//         n = maps.length - 1;
+//         m = maps[0].length - 1;
+//         ans = Integer.MAX_VALUE;
+
+//         maps[0][0] = 0;
+//         dfs(maps, 0, 0, 1);
+
+//         return ans == Integer.MAX_VALUE ? -1 : ans;
+//     }
+
+//     static void dfs(int[][] maps, int y, int x, int c) {
+        
+//         for (int d = 0; d < 4; d++) {
+//             int newY = y + dy[d];
+//             int newX = x + dx[d];
+//             int newC = c + 1;
+
+//             if (newY < 0 || newY > n || newX < 0 || newX > m || maps[newY][newX] == 0) continue;
+
+//             if (newY == n && newX == m) {
+//                 ans = Math.min(ans, newC);
+//             }
+
+//             maps[newY][newX] = 0;
+//             dfs(maps, newY, newX, newC);
+//             maps[newY][newX] = 1;
+//         }
+//     }
 
 }
