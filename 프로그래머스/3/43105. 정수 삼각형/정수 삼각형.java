887b7cbf4b11b865f2d17ab3ef7bd19a903854a5
Greta Jiyun Lee <51826219+howecofe@users.noreply.github.com>
2024-08-15 02:46:12 +0900

    [level 3] Title: 정수 삼각형, Time: 12.10 ms, Memory: 60.6 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
@@ -2,29 +2,24 @@ import java.util.*;
 
 class Solution {
     public int solution(int[][] triangle) {
-        List<List<int[]>> dp = new ArrayList<>();
-        for (int i = 0; i < triangle.length; i++) dp.add(new ArrayList<>());
 
-        dp.get(0).add(new int[]{triangle[0][0], 0});
+        for (int i = 1; i < triangle.length; i++) { // 현재 i층의 각 요소에 도달하는 비용 구하기
+            // i층의 첫번째 요소에서의 비용
+            triangle[i][0] += triangle[i - 1][0];
+            // i층의 마지막 요소에서의 비용
+            triangle[i][i] += triangle[i - 1][i - 1];
 
-        for (int i = 1; i < triangle.length; i++) { // 현재 i층에 도달하는 경로 비용 구하기
-            for (int j = 0; j < dp.get(i - 1).size(); j++) { // i-1 층의 각 경로 비용에 왼쪽 대각선, 오른쪽 대각선 비용 더하기
-                int num = dp.get(i - 1).get(j)[0];
-                int idx = dp.get(i - 1).get(j)[1];
-
-                // idx 기반으로 왼쪽 대각선, 오른쪽 대각선 비용 더하기
-                int left = triangle[i][idx];
-                int right = triangle[i][idx + 1];
-
-                dp.get(i).add(new int[]{num + left, idx});
-                dp.get(i).add(new int[]{num + right, idx + 1});
+            for (int j = 1; j < i; j++) {
+                int topLeft = triangle[i - 1][j - 1];
+                int topRight = triangle[i - 1][j];
+                triangle[i][j] += Math.max(topLeft, topRight); // i-1층(바로 위층)에서의 최댓값에 현재 위치 비용 더하기
             }
         }
 
         // 맨 마지막 층의 경로 비용 중 최댓값 구하기
-        List<int[]> last = dp.get(dp.size() - 1);
-        last.sort((o1, o2) -> o2[0] - o1[0]);
-        
-        return last.get(0)[0];
+        int[] last = triangle[triangle.length - 1];
+        Arrays.sort(last);
+
+        return last[last.length - 1];
     }
 }
