26d5cdda2b6d4d1dffbbe0986fcdd1fb563d30b6
Greta Jiyun Lee <51826219+howecofe@users.noreply.github.com>
2024-08-16 06:04:25 +0900

    [level 3] Title: 가장 먼 노드, Time: 42.66 ms, Memory: 115 MB -BaekjoonHub

---
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
@@ -0,0 +1,48 @@
+import java.util.*;
+
+class Solution {
+    static Map<Integer, Integer> costMap = new HashMap<>(); // 거리 : 개수 저장 맵
+    static List<List<Integer>> list = new ArrayList<>(); // 인접리스트
+    static boolean[] visited;
+    static Queue<int[]> q = new LinkedList<>();
+    
+    public static int solution(int n, int[][] edge) {
+        visited = new boolean[n + 1]; // 0: dummy
+        for (int i = 0; i <= n; i++) list.add(new ArrayList<>()); // 0: dummy
+
+        // 양방향 간선 표시
+        for (int[] e : edge) {
+            list.get(e[0]).add(e[1]);
+            list.get(e[1]).add(e[0]);
+        }
+
+        bfs();
+
+        // 가장 멀리 떨어진 노드의 개수 구하기
+        List<Integer> keys = new ArrayList<>(costMap.keySet());
+        keys.sort(Comparator.reverseOrder());
+
+        return costMap.get(keys.get(0));
+    }
+
+    static void bfs() {
+        // 1번 노드 넣고 방문 처리
+        q.offer(new int[]{1, 0}); // node, cost
+        visited[1] = true;
+
+        while (!q.isEmpty()) {
+            int[] tmp = q.poll();
+            int node = tmp[0];
+            int cost = tmp[1];
+            
+            costMap.put(cost, costMap.getOrDefault(cost, 0) + 1);
+
+            for (int n : list.get(node)) {
+                if (visited[n]) continue;
+
+                q.offer(new int[]{n, cost + 1});
+                visited[n] = true;
+            }
+        }
+    }
+}
