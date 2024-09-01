b46679c61a8a6e5dac07f5f0a3e1e46e525d57d3
Greta Jiyun Lee <51826219+howecofe@users.noreply.github.com>
2024-09-01 15:45:20 +0900

    [Silver II] Title: 최소 힙, Time: 272 ms, Memory: 27436 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+class Main {
+
+    public static void main(String[] args) throws Exception {
+//        System.setIn(new FileInputStream("res/input.txt"));
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        // 입력
+        int N = Integer.parseInt(br.readLine());
+
+        // 풀이
+        Queue<Integer> pq = new PriorityQueue<>((o1, o2) -> o1 - o2);
+
+        for (int i = 0; i < N; i++) {
+            int n = Integer.parseInt(br.readLine());
+
+            if (n != 0) {
+                pq.offer(n);
+                continue;
+            }
+
+            if (!pq.isEmpty()) bw.write(pq.poll() + "\n");
+            else bw.write(0 + "\n");
+        }
+
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+
+}
