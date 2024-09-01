347cf42ff88de091dac1e88ef60632962aa5edc1
Greta Jiyun Lee <51826219+howecofe@users.noreply.github.com>
2024-09-01 17:18:18 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 108 ms, Memory: 14448 KB -BaekjoonHub

---
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.java
@@ -0,0 +1,31 @@
+import java.io.*;
+
+class Main {
+
+    public static void main(String[] args) throws Exception {
+//        System.setIn(new FileInputStream("res/input.txt"));
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        // 입력
+        int N = Integer.parseInt(br.readLine());
+
+        // 풀이
+        int[] dp = new int[1001];
+        dp[1] = 1;
+        dp[2] = 3;
+
+        for (int i = 3; i <= N; i++) {
+            dp[i] = (dp[i - 1] + dp[i - 2] * 2) % 10007;
+        }
+
+        // 출력
+        bw.write(dp[N] + "");
+
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+
+}
