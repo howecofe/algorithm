a33d15e530bbcb948a67dacc5c19c58d942b79a2
Greta Jiyun Lee <51826219+howecofe@users.noreply.github.com>
2024-09-27 20:01:37 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 88.62 ms, Memory: 96.6 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,23 +1,23 @@
 import java.util.*;
 
 class Solution {
-    static Map<String, Integer> map = new HashMap<>();
-    static String answer;
+
     public String solution(String[] participant, String[] completion) {
-        // 마라톤에 참여한 선수들의 이름이 담긴 배열 participant
-        // 완주한 선수들의 이름이 담긴 배열 completion
-        // 단 한 명의 선수를 제외하고는 모든 선수가 마라톤을 완주
-        // 완주하지 못한 선수의 이름을 return
-
-        // 풀이
-        for (String p : participant) map.put(p, map.getOrDefault(p, 0) + 1);
-        for (String c : completion) map.put(c, map.get(c) - 1);
-
-        // 출력
-        for (Map.Entry<String, Integer> e : map.entrySet()) {
-            if (e.getValue() == 1) answer = e.getKey();
+        String answer = "";
+        Map<String, Integer> map = new HashMap<>();
+        
+        for (String p : participant) {
+            map.put(p, map.getOrDefault(p, 0) + 1);
         }
-
+        
+        for (String c : completion) {
+            map.put(c, map.get(c) - 1);
+        }
+        
+        for (String key : map.keySet()) {
+            if (map.get(key) != 0) answer = key;
+        }
+        
         return answer;
     }
 }
