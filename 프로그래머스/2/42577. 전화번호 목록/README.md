25e89b238b7a127739a0add956152f8c845660af
Greta Jiyun Lee <51826219+howecofe@users.noreply.github.com>
2024-09-29 16:10:33 +0900

    [level 2] Title: 전화번호 목록, Time: 343.12 ms, Memory: 98.9 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/README.md
+++ 프로그래머스/2/42577. 전화번호 목록/README.md
@@ -1,10 +1,10 @@
 # [level 2] 전화번호 목록 - 42577 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42577#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42577) 
 
 ### 성능 요약
 
-메모리: 98.7 MB, 시간: 340.04 ms
+메모리: 98.9 MB, 시간: 343.12 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 07월 23일 00:32:27
+2024년 09월 29일 16:10:31
 
 ### 문제 설명
 
--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -2,15 +2,12 @@ import java.util.*;
 
 class Solution {
     public boolean solution(String[] phone_book) {
-        // 전화번호를 담은 배열 phone_book
-        // 어떤 번호가 다른 번호의 접두어인 경우가 있으면 false를 그렇지 않으면 true를 return
 
-        // 풀이
         Arrays.sort(phone_book);
         for (int i = 0; i < phone_book.length - 1; i++) {
             if (phone_book[i + 1].startsWith(phone_book[i])) return false;
         }
-
+        
         return true;
     }
 }
