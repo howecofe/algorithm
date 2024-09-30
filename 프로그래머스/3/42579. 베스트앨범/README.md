3360816b0af6bd914fcedd75b5abcac1fb63bff0
Greta Jiyun Lee <51826219+howecofe@users.noreply.github.com>
2024-09-30 23:36:13 +0900

    [level 3] Title: 베스트앨범, Time: 1.96 ms, Memory: 70.3 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/README.md
+++ 프로그래머스/3/42579. 베스트앨범/README.md
@@ -1,10 +1,10 @@
 # [level 3] 베스트앨범 - 42579 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42579#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42579) 
 
 ### 성능 요약
 
-메모리: 72.4 MB, 시간: 2.04 ms
+메모리: 70.3 MB, 시간: 1.96 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 07월 23일 22:17:43
+2024년 09월 30일 23:36:09
 
 ### 문제 설명
 
--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
@@ -2,45 +2,47 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String[] genres, int[] plays) {
-        // 노래의 장르를 나타내는 문자열 배열 genres
-        // 노래별 재생 횟수를 나타내는 정수 배열 plays
-        // 베스트 앨범에 들어갈 노래의 고유 번호를 순서대로 return
-
-        // 총 재생횟수
-        Map<String, Integer> map = new HashMap<>();
-        for (int i = 0; i < genres.length; i++) map.put(genres[i], map.getOrDefault(genres[i], 0) + plays[i]);
-
-        // 재생횟수 높은 순으로 장르 구하기
-        List<String> list = new ArrayList<>(map.keySet());
-        list.sort((o1, o2) -> map.get(o2) - map.get(o1));
-
-        // 장르별 재생횟수 높은 2곡 구하기
+        Map<String, Integer> totalMap = new HashMap<>();
         List<Integer> ansList = new ArrayList<>();
+    
+        int len = genres.length;
+        for (int i = 0; i < len; i++) {
+            totalMap.put(genres[i], totalMap.getOrDefault(genres[i], 0) + plays[i]);
+        }
         
-        for (String gen : list) {
+        // 재생횟수 높은 순으로 장르 정렬
+        List<String> genList = new ArrayList<>(totalMap.keySet());
+        genList.sort((o1, o2) -> totalMap.get(o2) - totalMap.get(o1));
+        
+        // 장르별 고유번호-재생횟수 저장
+        for (String gen : genList) {
             Map<Integer, Integer> playMap = new HashMap<>();
-            for (int i = 0; i < genres.length; i++) {
-                // <고유번호, 재생횟수> 저장
-                if (genres[i].equals(gen)) playMap.put(i, plays[i]);
+            
+            for (int i = 0; i < len; i++) {
+                if (genres[i].equals(gen)) {
+                    playMap.put(i, plays[i]);
+                }
             }
-
-            // playMap value 내림차순 -> 같으면 key 오름차순
-            List<Integer> playList = new ArrayList<>(playMap.keySet());
-            playList.sort((o1, o2) -> playMap.get(o2) - playMap.get(o1) == 0? o1 - o2 : playMap.get(o2) - playMap.get(o1));
-
-            // 상위 2곡 추가
-            if (playList.size() == 1) {
-                ansList.add(playList.get(0));
+            
+            List<Integer> idxList = new ArrayList<>(playMap.keySet());
+            idxList.sort((o1, o2) -> playMap.get(o2) == playMap.get(o1)?
+                        o1 - o2 : playMap.get(o2) - playMap.get(o1));
+            
+            if (idxList.size() == 1) {
+                ansList.add(idxList.get(0));
             } else {
-                ansList.add(playList.get(0));
-                ansList.add(playList.get(1));
+                ansList.add(idxList.get(0));
+                ansList.add(idxList.get(1));
             }
         }
         
         // list -> array
-        int[] ans = new int[ansList.size()];
-        for (int i = 0; i < ansList.size(); i++) ans[i] = ansList.get(i);
-
-        return ans;
+        int[] answer = new int[ansList.size()];
+        for (int i = 0; i < answer.length; i++) {
+            answer[i] = ansList.get(i);
+        }
+    
+        return answer;
+        
     }
 }
