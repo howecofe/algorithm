793a8c4328e84ac5a150ee37c665f30cb24b4ef2
Greta Jiyun Lee <51826219+howecofe@users.noreply.github.com>
2024-10-04 04:26:17 +0900

    [level 2] Title: 프로세스, Time: 1.11 ms, Memory: 77.1 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.java
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -1,36 +1,43 @@
 import java.util.*;
 
 class Solution {
-    static int cnt;
-    static LinkedList<Integer> pq = new LinkedList<>(); // 우선순위 담는 큐
-    static LinkedList<Integer> iq = new LinkedList<>(); // idx 담는 큐
-    public int solution(int[] priorities, int location) {
-        // 프로세스의 중요도가 순서대로 담긴 배열 priorities : 숫자가 클수록 우선순위가 높다.
-        // 몇 번째로 실행되는지 알고싶은 프로세스의 위치를 알려주는 location
-        // 해당 프로세스가 몇 번째로 실행되는지 return
-
-        for (int i = 0; i < priorities.length; i++) {
-            pq.offer(priorities[i]);
-            iq.offer(i);
+    
+    class Node {
+        int idx;
+        int score;
+        
+        Node(int idx, int score) {
+            this.idx = idx;
+            this.score = score;
         }
-
+    }
+    
+    public int solution(int[] priorities, int location) {
+        int cnt = 0;
+        LinkedList<Node> q = new LinkedList<>();
+        
+        for (int i = 0; i < priorities.length; i++) {
+            q.add(new Node(i, priorities[i]));
+        }
+        
         Arrays.sort(priorities);
+        
         for (int i = priorities.length - 1; i >= 0; i--) {
-            while (!pq.isEmpty()) {
-                int p = pq.poll();
-                int idx = iq.poll();
-
-                if (p == priorities[i]) {
+            int max = priorities[i];
+            
+            while (!q.isEmpty()) {
+                Node node = q.poll();
+                if (node.score == max) {
                     cnt++;
-                    if (idx == location) return cnt;
+                    if (node.idx == location) return cnt;
                     break;
                 } else {
-                    pq.offer(p);
-                    iq.offer(idx);
+                    q.add(node);
                 }
             }
+            
         }
-
+        
         return cnt;
     }
 }
