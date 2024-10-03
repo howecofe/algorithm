9e03203256e37746c062cc7dbe1663ecef26376b
Greta Jiyun Lee <51826219+howecofe@users.noreply.github.com>
2024-10-03 17:34:31 +0900

    [level 2] Title: 올바른 괄호, Time: 17.42 ms, Memory: 53.8 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -1,25 +1,25 @@
-import java.lang.reflect.Array;
 import java.util.*;
 
 class Solution {
-    static ArrayDeque<Character> stk = new ArrayDeque<>();
-    static char[] arr;
+   
     boolean solution(String s) {
-        // '(' 또는 ')' 로만 이루어진 문자열 s
-        // s가 올바른 괄호이면 true를 return 하고, 올바르지 않은 괄호이면 false를 return
-
-        arr = s.toCharArray();
+        char[] arr = s.toCharArray();
+        Deque<Character> stk = new ArrayDeque<>();
+        
         for (char c : arr) {
-            if (c == '(') stk.push(c);
-            else { // c == ')'
-                if (stk.isEmpty() || stk.peek() == ')') return false;
-                
-                stk.pop(); // '('이면 꺼내기
+            if (c == '(') {
+                stk.push(c);
+                continue;
             }
+            
+            // c == ')'
+            if (!stk.isEmpty() && stk.peek() == '(') stk.pop();
+            else return false;
+            
         }
-
+        
         if (!stk.isEmpty()) return false;
-
+        
         return true;
     }
 }
