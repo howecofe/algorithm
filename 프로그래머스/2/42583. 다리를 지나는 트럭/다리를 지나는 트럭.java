f42e60c5989a43f1f815cba7f4c50813004c4201
Greta Jiyun Lee <51826219+howecofe@users.noreply.github.com>
2024-10-04 22:35:30 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 136.13 ms, Memory: 123 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
@@ -1,30 +1,58 @@
 import java.util.*;
 
 class Solution {
-    public int solution(int bridge_length, int weight, int[] truck_weights) {
-        LinkedList<Integer[]> brQ = new LinkedList<>(); // 다리 위 트럭 큐
-        LinkedList<Integer> waitQ = new LinkedList<>(); // 대기 트럭 큐
-        for (int w : truck_weights) waitQ.offer(w);
-
-        int brW = 0; // 다리 위의 트럭들의 무게
-        int time = 0;
-
-        while (!waitQ.isEmpty() || !brQ.isEmpty()) {
-            // 트럭이 나가야 하는 경우
-            if (!brQ.isEmpty() && brQ.peek()[1] == time) {
-                brW -= brQ.poll()[0];
-            }
-
-            // 새로운 트럭을 추가했을 때 무게와 다리 길이를 초과하지 않으면 추가
-            if (!waitQ.isEmpty() && brW + waitQ.peek() <= weight && brQ.size() + 1 <= bridge_length) {
-                int w = waitQ.poll();
-                brQ.offer(new Integer[]{w, time + bridge_length}); // (트럭 무게, 트럭이 큐에서 나가야 하는 시간)
-                brW += w;
-            }
-
-            time++;
+    
+    class Truck {
+        int w, c;
+        
+        Truck(int w, int c) {
+            this.w = w;
+            this.c = c;
         }
-
+    }
+    
+    public int solution(int bridge_length, int weight, int[] truck_weights) {
+        int time = 0;
+        LinkedList<Integer> truck = new LinkedList<>();
+        LinkedList<Truck> bridge = new LinkedList<>();
+        int currWeight = 0;
+        
+        for (int t : truck_weights) truck.add(t);
+        
+        // 첫번째 트럭을 다리 위로 올리기
+        int init = truck.poll();
+        bridge.add(new Truck(init, 1));
+        currWeight += init;
+        time++;
+        
+        while (!bridge.isEmpty()) {
+            time++;
+            
+            // 첫번째 트럭이 다리 끝에 도착했다면 방출
+            Truck first = bridge.peek();
+            if (first.c == bridge_length) {
+                bridge.poll();
+                currWeight -= first.w;
+            }
+            
+            // 현재 다리 위의 모든 트럭들 카운트 증가
+            for (int i = 0; i < bridge.size(); i++) {
+                Truck t = bridge.poll();
+                bridge.add(new Truck(t.w, t.c + 1));
+            }
+            
+            // 다음 트럭이 올라갈 수 있는 경우, 다음 트럭 올리기
+            if (bridge.size() < bridge_length) { // 다리가 꽉 차지 않은 경우
+                // 무게를 초과하지 않는 경우
+                if (!truck.isEmpty() && currWeight + truck.peek() <= weight) { 
+                    int w = truck.poll();
+                    bridge.add(new Truck(w, 1));
+                    currWeight += w;
+                }
+                
+            }
+        }
+        
         return time;
     }
 }
