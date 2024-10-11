65241d3f8d74ed2e61331c4a6c7e5e8319532a62
Greta Jiyun Lee <51826219+howecofe@users.noreply.github.com>
2024-10-12 01:11:17 +0900

    [level 2] Title: 피로도, Time: 14.92 ms, Memory: 80.8 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -1,35 +1,44 @@
 import java.util.*;
 
 class Solution {
-    static int max;
+    
     static boolean[] visited;
     static int[][] dg;
+    static int len, answer;
     
     public int solution(int k, int[][] dungeons) {
-        dg = dungeons.clone();
-        visited = new boolean[dg.length];
-
+        len = dungeons.length;
+        visited = new boolean[len];
+        dg = new int[len][];
+        
+        for (int i = 0; i < len; i++) {
+            dg[i] = dungeons[i].clone();
+        }
+        
         dfs(0, 0, k);
-
-        return max;
+        
+        return answer;
     }
-
-    static void dfs(int depth, int cnt, int k) {
-        if (depth == dg.length) {
-            max = Math.max(max, cnt);
+    
+    static void dfs(int depth, int cnt, int power) {
+        if (depth == len) {
+            answer = Math.max(answer, cnt);
             return;
         }
-
-        for (int i = 0; i < dg.length; i++) {
+        
+        for (int i = 0; i < len; i++) {
             if (visited[i]) continue;
-
+            
             visited[i] = true;
-            if (dg[i][0] <= k) {
-                dfs(depth + 1, cnt + 1, k - dg[i][1]);
+            
+            if (dg[i][0] <= power) {
+                dfs(depth + 1, cnt + 1, power - dg[i][1]);
             } else {
-                dfs(depth + 1, cnt, k);
+                dfs(depth + 1, cnt, power);
             }
+            
             visited[i] = false;
         }
     }
+
 }
