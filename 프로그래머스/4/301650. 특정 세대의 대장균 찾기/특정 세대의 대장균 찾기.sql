343d94250c9d1730919f6f1a6c7fcbd3dd9b1af0
Greta Jiyun Lee <51826219+howecofe@users.noreply.github.com>
2024-10-16 01:29:50 +0900

    [level 4] Title: 특정 세대의 대장균 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/301650. 특정 세대의 대장균 찾기/특정 세대의 대장균 찾기.sql
+++ 프로그래머스/4/301650. 특정 세대의 대장균 찾기/특정 세대의 대장균 찾기.sql
@@ -1,13 +1,6 @@
-SELECT ID
-FROM ECOLI_DATA
-WHERE PARENT_ID IN (
-    SELECT ID
-    FROM ECOLI_DATA
-    WHERE PARENT_ID IN (
-        SELECT ID
-        FROM ECOLI_DATA
-        WHERE PARENT_ID IS NULL
-    )
-)
-ORDER BY ID;
-    
+select CC.ID
+from
+    ECOLI_DATA CC join ECOLI_DATA C on CC.PARENT_ID = C.ID
+    join ECOLI_DATA P on C.PARENT_ID = P.ID
+where P.PARENT_ID is null
+order by CC.ID;
