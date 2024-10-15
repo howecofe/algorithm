de1b6616e75fff2a632335bf4249ba04e23b8179
Greta Jiyun Lee <51826219+howecofe@users.noreply.github.com>
2024-10-16 00:00:05 +0900

    [level 2] Title: 3월에 태어난 여성 회원 목록 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/131120. 3월에 태어난 여성 회원 목록 출력하기/3월에 태어난 여성 회원 목록 출력하기.sql
+++ 프로그래머스/2/131120. 3월에 태어난 여성 회원 목록 출력하기/3월에 태어난 여성 회원 목록 출력하기.sql
@@ -1,10 +1,6 @@
-select
-    MEMBER_ID, MEMBER_NAME, GENDER, DATE_FORMAT(DATE_OF_BIRTH, '%Y-%m-%d')
-from
-    MEMBER_PROFILE
-where
-    DATE_OF_BIRTH like '%-03-%'
-    and GENDER = 'W'
-    and TLNO is not null
-order by
-    MEMBER_ID;
+select MEMBER_ID, MEMBER_NAME, GENDER, date_format(DATE_OF_BIRTH, '%Y-%m-%d') DATE_OF_BIRTH
+from MEMBER_PROFILE
+where DATE_OF_BIRTH like '%-03-%'
+and GENDER = 'W'
+and TLNO is not null
+order by MEMBER_ID;
