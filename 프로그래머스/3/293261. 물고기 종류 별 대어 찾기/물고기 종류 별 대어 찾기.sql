e40f8e3e7ec6bc0ce4f6e82b9debcfdc607513eb
Greta Jiyun Lee <51826219+howecofe@users.noreply.github.com>
2024-10-16 01:52:35 +0900

    [level 3] Title: 물고기 종류 별 대어 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/3/293261. 물고기 종류 별 대어 찾기/물고기 종류 별 대어 찾기.sql
+++ 프로그래머스/3/293261. 물고기 종류 별 대어 찾기/물고기 종류 별 대어 찾기.sql
@@ -1,3 +1,25 @@
+# select A.ID, B.FISH_NAME, A.LENGTH
+# from
+#     FISH_INFO A join FISH_NAME_INFO B on A.FISH_TYPE = B.FISH_TYPE,
+#     (select FISH_TYPE, MAX(LENGTH) LENGTH
+#     from FISH_INFO
+#     group by FISH_TYPE) C
+# where A.FISH_TYPE = C.FISH_TYPE
+# and A.LENGTH = C.LENGTH
+# order by A.ID;
+
+select A.ID, B.FISH_NAME, A.LENGTH
+from
+    FISH_INFO A join FISH_NAME_INFO B on A.FISH_TYPE = B.FISH_TYPE
+where
+    (A.FISH_TYPE, A.LENGTH) in (
+        select FISH_TYPE, MAX(LENGTH)
+        from FISH_INFO
+        group by FISH_TYPE
+    )
+order by A.ID;
+
+
 # SELECT
 #     D.ID,
 #     C.FISH_NAME,
@@ -21,18 +43,18 @@
 # ORDER BY
 #     D.ID;
     
-SELECT
-    A.ID,
-    B.FISH_NAME,
-    A.LENGTH
-FROM
-    FISH_INFO AS A
-    JOIN FISH_NAME_INFO AS B
-    ON A.FISH_TYPE = B.FISH_TYPE
-WHERE
-    (A.FISH_TYPE, A.LENGTH) IN (
-        SELECT FISH_TYPE, MAX(LENGTH)
-        FROM FISH_INFO
-        GROUP BY FISH_TYPE)
-ORDER BY
-    A.ID;
+# SELECT
+#     A.ID,
+#     B.FISH_NAME,
+#     A.LENGTH
+# FROM
+#     FISH_INFO AS A
+#     JOIN FISH_NAME_INFO AS B
+#     ON A.FISH_TYPE = B.FISH_TYPE
+# WHERE
+#     (A.FISH_TYPE, A.LENGTH) IN (
+#         SELECT FISH_TYPE, MAX(LENGTH)
+#         FROM FISH_INFO
+#         GROUP BY FISH_TYPE)
+# ORDER BY
+#     A.ID;
