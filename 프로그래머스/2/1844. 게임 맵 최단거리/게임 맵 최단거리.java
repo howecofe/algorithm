feadb89327deeeb690bf78ec528be65c55df374b
Greta Jiyun Lee <51826219+howecofe@users.noreply.github.com>
2024-10-18 23:50:32 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 7.64 ms, Memory: 56.4 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,90 +1,54 @@
 import java.util.*;
 
 class Solution {
-    
-    // 1. BFS (통과)
-    static Queue<Node> q = new LinkedList<>();
-    static int ans, n, m;
-    static int[] dy = {1, 0, -1, 0};
+    static int n, m, answer;
+    static int[] dy = {1, 0, -1, 0}; // 하 우 상 좌
     static int[] dx = {0, 1, 0, -1};
+    static Queue<Node> q = new LinkedList<>();
     
-    public int solution(int[][] maps) {
-        n = maps.length - 1;
-        m = maps[0].length - 1;
-        ans = -1;
-
-        bfs(maps);
-
-        return ans;
-    }
-
     static class Node {
         int y, x, c;
+        
         Node(int y, int x, int c) {
             this.y = y;
             this.x = x;
             this.c = c;
         }
     }
-
+    
+    public int solution(int[][] maps) {
+        answer = -1;
+        n = maps.length - 1;
+        m = maps[0].length - 1;
+        
+        bfs(maps);
+        
+        return answer;
+    }
+    
     static void bfs(int[][] maps) {
         q.offer(new Node(0, 0, 1));
         maps[0][0] = 0;
-
+        
         while (!q.isEmpty()) {
             Node node = q.poll();
-
+            
             for (int d = 0; d < 4; d++) {
-                int newY = node.y + dy[d];
-                int newX = node.x + dx[d];
-                int cost = node.c + 1;
-
-                if (newY < 0 || newY > n || newX < 0 || newX > m || maps[newY][newX] == 0) continue;
-
-                if (newY == n && newX == m) {
-                    ans = cost;
+                int ny = node.y + dy[d];
+                int nx = node.x + dx[d];
+                int nc = node.c + 1;
+                
+                if (ny < 0 || nx < 0 || ny > n || nx > m || maps[ny][nx] == 0) continue;
+                
+                if (ny == n && nx == m) {
+                    answer = nc;
                     return;
                 }
-
-                maps[newY][newX] = 0;
-                q.offer(new Node(newY, newX, cost));
+                
+                q.offer(new Node(ny, nx, nc));
+                maps[ny][nx] = 0;
             }
         }
     }
-    
-    // 2. DFS (시간초과)
-//     static int ans, n, m;
-//     static int[] dy = {1, 0, -1, 0};
-//     static int[] dx = {0, 1, 0, -1};
-    
-//     public int solution(int[][] maps) {
-//         n = maps.length - 1;
-//         m = maps[0].length - 1;
-//         ans = Integer.MAX_VALUE;
-
-//         maps[0][0] = 0;
-//         dfs(maps, 0, 0, 1);
-
-//         return ans == Integer.MAX_VALUE ? -1 : ans;
-//     }
-
-//     static void dfs(int[][] maps, int y, int x, int c) {
-        
-//         for (int d = 0; d < 4; d++) {
-//             int newY = y + dy[d];
-//             int newX = x + dx[d];
-//             int newC = c + 1;
-
-//             if (newY < 0 || newY > n || newX < 0 || newX > m || maps[newY][newX] == 0) continue;
-
-//             if (newY == n && newX == m) {
-//                 ans = Math.min(ans, newC);
-//             }
-
-//             maps[newY][newX] = 0;
-//             dfs(maps, newY, newX, newC);
-//             maps[newY][newX] = 1;
-//         }
-//     }
 
 }
