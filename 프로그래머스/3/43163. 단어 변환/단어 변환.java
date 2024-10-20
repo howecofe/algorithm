534b116aa4b592831e762d1c0e529491997d6b8d
Greta Jiyun Lee <51826219+howecofe@users.noreply.github.com>
2024-10-20 17:43:20 +0900

    [level 3] Title: 단어 변환, Time: 0.46 ms, Memory: 74.2 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,124 +1,40 @@
 import java.util.*;
 
 class Solution {
-    // 1. BFS
-//     static int ans;
-//     static Queue<Word> q = new LinkedList<>();
-//     static boolean[] visited;
-//     static class Word {
-//         String w;
-//         int c;
-//         Word(String w, int c) {
-//             this.w = w;
-//             this.c = c;
-//         }
-//     }
-
-//     public int solution(String begin, String target, String[] words) {
-//         visited = new boolean[words.length];
-
-//         // target 단어가 없는 경우
-//         boolean exists = false;
-//         for (String w : words) {
-//             if (w.equals(target)) {
-//                 exists = true;
-//                 break;
-//             }
-//         }
-//         if (!exists) return 0;
-
-//         q.offer(new Word(begin, 0));
-//         bfs(words, target);
-
-//         if (ans == 0) return 0; // 바꿀 수 없는 경우
-
-//         return ans;
-//     }
-
-//     static void bfs(String[] words, String target) {
-
-//         while (!q.isEmpty()) {
-//             Word word = q.poll();
-//             String before = word.w;
-//             int cost = word.c;
-
-//             for (int i = 0; i < words.length; i++) {
-//                 if (visited[i]) continue;
-
-//                 // 바꿀 수 있는지 단어인지 판별
-//                 String after = words[i];
-//                 int diffCnt = 0;
-//                 boolean changable = true;
-//                 for (int j = 0; j < before.length(); j++) {
-//                     if (before.charAt(j) != after.charAt(j)) diffCnt++;
-//                     if (diffCnt > 1) {
-//                         changable = false;
-//                         break;
-//                     }
-//                 }
-
-//                 if (!changable) continue;
-
-//                 // 바꿀 수 있는 경우
-//                 // target 단어인지 체크
-//                 if (after.equals(target)) {
-//                     ans = cost + 1;
-//                     return;
-//                 }
-//                 q.offer(new Word(after, cost + 1));
-//                 visited[i] = true;
-//             }
-//         }
-//     }
-    
-    // 2. DFS
-    static int ans = Integer.MAX_VALUE;
     static boolean[] visited;
-
+    static int answer;
+    
     public int solution(String begin, String target, String[] words) {
         visited = new boolean[words.length];
-
-        // target 단어가 없는 경우
-        boolean exists = false;
-        for (String w : words) {
-            if (w.equals(target)) {
-                exists = true;
-                break;
-            }
-        }
-        if (!exists) return 0;
-
-        dfs(words, target, begin, 0);
-
-        if (ans == Integer.MAX_VALUE) return 0; // 바꿀 수 없는 경우
-
-        return ans;
+        answer = Integer.MAX_VALUE;
+        
+        dfs(begin, target, words, 0);
+        
+        if (answer == Integer.MAX_VALUE) return 0;
+        
+        return answer;
     }
-
-    static void dfs(String[] words, String target, String before, int cnt) {
-
+    
+    static void dfs(String before, String target, String[] words, int cnt) {
+        if (before.equals(target)) {
+            answer = Math.min(answer, cnt);
+            return;
+        }
+        
         for (int i = 0; i < words.length; i++) {
             if (visited[i]) continue;
-
-            // 바꿀 수 있는 단어인지 판별
+            
             int diffCnt = 0;
-            boolean changable = true;
-            String after = words[i];
+            
             for (int j = 0; j < before.length(); j++) {
-                if (before.charAt(j) != after.charAt(j)) diffCnt++;
-                if (diffCnt > 1) {
-                    changable = false;
-                    break;
-                }
+                if (before.charAt(j) != words[i].charAt(j)) diffCnt++;
             }
-            if (!changable) continue;
-
-            // 바꿀 수 있는 경우
-            if (after.equals(target)) ans = Math.min(ans, cnt + 1); // target 단어 체크
-            visited[i] = true;
-            dfs(words, target, after, cnt + 1);
-            visited[i] = false;
+            
+            if (diffCnt == 1) {
+                visited[i] = true;
+                dfs(words[i], target, words, cnt + 1);
+                visited[i] = false;
+            }
         }
-
     }
 }
