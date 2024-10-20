17ea092a797fd9e7bdbf80eb0472f5d39cd8f54e
Greta Jiyun Lee <51826219+howecofe@users.noreply.github.com>
2024-10-20 18:39:19 +0900

    [level 3] Title: 여행경로, Time: 333.79 ms, Memory: 248 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/README.md
+++ 프로그래머스/3/43164. 여행경로/README.md
@@ -1,10 +1,10 @@
 # [level 3] 여행경로 - 43164 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43164#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43164) 
 
 ### 성능 요약
 
-메모리: 257 MB, 시간: 305.09 ms
+메모리: 248 MB, 시간: 333.79 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 08월 09일 23:17:04
+2024년 10월 20일 18:39:17
 
 ### 문제 설명
 
--- 프로그래머스/3/43164. 여행경로/여행경로.java
+++ 프로그래머스/3/43164. 여행경로/여행경로.java
@@ -1,69 +1,61 @@
 import java.util.*;
 
 class Solution {
-    static boolean[] visited;
-    static Map<String, List<String>> map = new HashMap<>();
-    static Map<String, List<Integer>> tktIdxMap = new HashMap<>();
-    static List<String> ansList = new ArrayList<>();
     static int ansLen;
-
+    static boolean[] visited;
+    static List<String> ansList = new ArrayList<>();
+    static Map<String, List<String>> map = new HashMap<>();
+    static Map<String, List<Integer>> idxMap = new HashMap<>();
+    
     public String[] solution(String[][] tickets) {
         visited = new boolean[tickets.length];
         ansLen = tickets.length + 1;
-
-        // key: 출발 공항, value: 도착 공항
+        
         for (int i = 0; i < tickets.length; i++) {
             map.putIfAbsent(tickets[i][0], new ArrayList<>());
             map.get(tickets[i][0]).add(tickets[i][1]);
-            // 중복 티켓이 있을 수 있으므로 인덱스를 리스트에 추가
-            tktIdxMap.putIfAbsent(Arrays.toString(tickets[i]), new ArrayList<>());
-            tktIdxMap.get(Arrays.toString(tickets[i])).add(i);
+            
+            String key = Arrays.toString(tickets[i]);
+            idxMap.putIfAbsent(key, new ArrayList<>());
+            idxMap.get(key).add(i);
         }
-
-        // value 오름차순 정렬
+        
         for (List<String> list : map.values()) Collections.sort(list);
-
-        // ICN 공항부터 방문 시작
+        
         List<String> pathList = new ArrayList<>();
         pathList.add("ICN");
         dfs("ICN", pathList);
-
+        
         // list -> array
-        String[] ans = new String[ansLen];
-        for (int i = 0; i < ans.length; i++) ans[i] = ansList.get(i);
-
-        return ans;
+        String[] answer = new String[ansList.size()];
+        for (int i = 0; i < answer.length; i++) answer[i] = ansList.get(i);
+        
+        return answer;
     }
-
-    static void dfs(String start, List<String> pathList) {
-        if (pathList.size() == ansLen) {
-            if (ansList.isEmpty()) { // 최초에만 list 복사
-                ansList = new ArrayList<>(pathList);
+    
+    static void dfs(String start, List<String> list) {
+        if (list.size() == ansLen) {
+            if (ansList.isEmpty()) {
+                ansList = new ArrayList<>(list);
             }
             return;
         }
-
-        List<String> ends = map.get(start);
-        if (ends == null) return;
-
-        for (String end : ends) {
-            int idx = -1;
-            boolean canVisit = false;
-            // 방문하지 않은 티켓인지 확인
-            for (int i : tktIdxMap.get(Arrays.toString(new String[]{start, end}))) {
-                if (visited[i]) continue;
-                canVisit = true;
-                idx = i;
+        
+        if (!map.containsKey(start)) return;
+        
+        for (String end : map.get(start)) {
+            // 방문 여부 체크
+            for (int idx : idxMap.get(Arrays.toString(new String[]{start, end}))) {
+                if (visited[idx]) continue;
+                
+                visited[idx] = true;
+                list.add(end);
+                dfs(end, list);
+                list.remove(list.size() - 1); // backtrack
+                visited[idx] = false; // backtrack
                 break;
             }
-
-            if (!canVisit) continue;
-
-            visited[idx] = true;
-            pathList.add(end);
-            dfs(end, pathList);
-            pathList.remove(pathList.size() - 1); // backtrack
-            visited[idx] = false; // backtrack
         }
     }
+
 }
